affb3855a58b5f8d7f8d6aede2ff0511062331fa
Neal Richardson <neal@crunch.io>
2014-02-28 18:03:28 -0800

    removing all_variables from mocks and adding hierarchical order

--- pkg/inst/tests/test-mock-api.R
+++ pkg/inst/tests/test-mock-api.R
@@ -1,7 +1,7 @@
 context("Mock!")
 
 with(fake.HTTP, {
-    ds <- as.dataset(GET("datasets/dataset1.json"))
+    ds <- as.dataset(GET("api/datasets/dataset1.json"))
     expect_true(is.dataset(ds))
     expect_true(is.Categorical(ds$gender))
     expect_true(is.Numeric(ds$birthyr))
