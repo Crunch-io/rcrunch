47b25b273b4806424fc3140d4b7d9be4ac4b5951
Neal Richardson <neal@crunch.io>
2014-03-28 15:07:12 -0700

    unbind, but finding bugs with [#68132230]

--- pkg/inst/tests/test-make-array.R
+++ pkg/inst/tests/test-make-array.R
@@ -30,6 +30,14 @@ if (!run.only.local.tests) {
                 expect_true(is.CA(var))
                 testdf <- refresh(testdf)
                 expect_equal(c("test1", "v4"), names(testdf))
+                ## unbind.
+                skip({
+                    u <- try(unbind(testdf$test1))
+                    expect_false(is.error(u))
+                    testdf <- refresh(testdf)
+                    expect_identical(names(testdf), names(mrdf))
+                    expect_identical(ncol(testdf), 4L)
+                })
             })
         })
         
@@ -97,6 +105,16 @@ if (!run.only.local.tests) {
                 
                 var <- undichotomize(var)
                 expect_true(is.CA(var))
+                
+                ## unbind.
+                skip({
+                    u <- try(unbind(var))
+                    expect_false(is.error(u))
+                    testdf <- refresh(testdf)
+                    print(getDatasetVariables(testdf))
+                    expect_identical(names(testdf), names(mrdf))
+                    expect_identical(ncol(testdf), 4L)
+                })
             })
             
             with(test.dataset(mrdf), {
