bb4a18c4fdec613ba7209ae54d8ec19fcf4b012b
Neal Richardson <neal@crunch.io>
2014-06-09 12:27:43 -0700

    function to create a mock dataset of arrays for testing

--- pkg/inst/tests/test-timing.R
+++ pkg/inst/tests/test-timing.R
@@ -15,3 +15,19 @@ if (!run.only.local.tests) {
     })
 }
 })
+
+
+mockArrayDataset <- function (nvars, nsubvars=5, nrows=3) {
+    ds <- rcrunch:::createDataset(name=paste("Arrays", rcrunch:::now()))
+    for (i in as.vector(outer(letters, letters, FUN=paste0))[seq_len(nvars)]) {
+        rcrunch:::POSTNewVariable(ds@urls$variables_url, list(
+            name=i,
+            type="categorical_array",
+            subvariables=lapply(seq_len(nsubvars),
+                function (x) toVariable(as.factor(seq_len(nrows)), 
+                    name=paste(i, x, sep="_")))
+        ))
+    }
+    invisible(refresh(ds))
+}
+
