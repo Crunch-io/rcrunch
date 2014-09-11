b2a6f50472a668a9b56bfff9ba9c844bc5edeaa7
Neal Richardson <neal@crunch.io>
2014-09-11 17:40:19 -0400

    more detailed test messages on the failing derive tests

--- pkg/tests/testthat/test-derive.R
+++ pkg/tests/testthat/test-derive.R
@@ -3,13 +3,18 @@ context("Derive a new variable")
 if (run.integration.tests) {
     with(test.authentication, {
         with(test.dataset(df), {
+            try(ds$v3a <- ds$v3 + 5)
             test_that("Can derive a numeric from a numeric", {
-                try(ds$v3a <- ds$v3 + 5)
                 expect_true(is.Numeric(ds$v3a))
                 expect_identical(as.vector(ds$v3a), as.vector(ds$v3) + 5)
-                ## Now update v3's values and confirm that v3a is still linked
-                try(ds$v3 <- df$v3 + 7)
+            })
+            
+            ## Now update v3's values and confirm that v3a is still linked
+            try(ds$v3 <- df$v3 + 7)
+            test_that("The source variable was in fact updated", {
                 expect_equivalent(as.vector(ds$v3), df$v3 + 7)
+            })
+            test_that("A derived numeric is updated when its source variable is modified", {
                 expect_identical(as.vector(ds$v3a), as.vector(ds$v3) + 5)
                 expect_equivalent(as.vector(ds$v3a), df$v3 + 12)
             })
