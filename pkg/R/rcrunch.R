e38382ad178137852648748c2ad8f6ead81e6b7a
Neal Richardson <neal@crunch.io>
2015-01-06 14:42:03 -0800

    Update DESCRIPTION and add package doc

--- pkg/R/rcrunch.R
+++ pkg/R/rcrunch.R
@@ -1,3 +1,21 @@
+##' Crunch.io: instant, visual, collaborative data analysis
+##'
+##' \href{http://crunch.io/}{Crunch.io} provides a cloud-based data store and
+##' analytic engine. It has a \href{https://beta.crunch.io/}{web client} for
+##' interactive data exploration and visualization. The rcrunch package for R
+##' allows analysts to interact with and manipulate Crunch datasets from within
+##' R. Importantly, this allows technical researchers to collaborate naturally
+##' with team members, managers, and clients who prefer a point-and-click
+##' interface: because all connect to the same dataset in the cloud, there is no
+##' need to email files back and forth continually to share results. 
+##'
+##' @seealso To learn more about using the package, see
+##' \code{vignette("getting-started", package="rcrunch")}. To sign up for a
+##' Crunch.io account, visit \url{https://beta.crunch.io/}.
+##' @docType package
+##' @name rcrunch
+NULL
+
 ##' @importFrom httr set_config
 .onAttach <- function (lib, pkg="pkg") {
     if (is.null(getOption("crunch.api"))) {
