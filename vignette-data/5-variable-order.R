4a8e9a6365e4a942fb98bb057ec991eac5b34f33
Neal Richardson <neal@crunch.io>
2015-04-22 16:49:36 -0400

    First part of a variable order vignette

--- vignette-data/5-variable-order.R
+++ vignette-data/5-variable-order.R
@@ -7,4 +7,19 @@ login()
 
 load("../pkg/vignettes/array-variables.RData")
 
+step0 <- ordering(ds)
+
+ordering(ds) <- VariableOrder(
+        VariableGroup("Demos", ds[c(1:3, 93:118)]),
+        VariableGroup("Tracking questions", ds[c(4,5,52:92)]),
+        VariableGroup("This week", ds[6:51])
+    )
+step1 <- ordering(ds)
+
+names(ordering(ds))[1] <- "Demographics"
+step2 <- ordering(ds)
+
+ordering(ds) <- ordering(ds)[c(2, 3, 1)]
+step3 <- ordering(ds)
+
 save.image(file="../pkg/vignettes/variable-order.RData")
