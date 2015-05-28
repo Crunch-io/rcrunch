b3f58c400c244f064c4b1795e6fd90ac7e75eeb8
Neal Richardson <neal@crunch.io>
2015-05-27 18:55:01 -0700

    Notes on test failures

--- tests/testthat/test-replace-batch.R
+++ tests/testthat/test-replace-batch.R
@@ -8,7 +8,7 @@ if (run.integration.tests) {
         df2$v6 <- NULL ## Drop a variable
         with(test.dataset(df, "d1"), {
             with(test.dataset(df2, "d2"), {
-                test_that("Show that the two datasets are different", {
+                test_that("The two datasets are different", {
                     expect_false(nrow(d1) == nrow(d2))
                     expect_false(ncol(d1) == ncol(d2))
                     expect_false(all(levels(as.vector(d1$v4)) == levels(as.vector(d2$v4))))
@@ -20,10 +20,10 @@ if (run.integration.tests) {
                     expect_identical(self(out), self(d1))
                     d1 <- refresh(d1)
                     expect_identical(nrow(d1), nrow(d2))
-                    expect_identical(ncol(d1), ncol(d2))
+                    expect_identical(ncol(d1), ncol(d2)) ## Fails. Because:
+                    expect_true(is.null(d1$v6)) ## also fails. Variable still exists
                     expect_identical(as.vector(d1$v3), as.vector(d2$v3))
-                    expect_identical(as.vector(d1$v4), as.vector(d2$v4))
-                    expect_true(is.null(d1$v6))
+                    expect_identical(as.vector(d1$v4), as.vector(d2$v4)) ## Fails because categories contain not just "C" and "D" from d2 but "B" from the old d1
                     
                     ## Questions:
                     # 1) If I replace a batch and exclude a variable, should the variable disappear? (Currently, its values are just being NA'd)
