4199d36d07cac8b92ec1038c331c9414f9619e7b
Robert Brewer <fumanchu@aminus.org>
2015-07-02 11:49:09 -0700

    Updating to new standardized ragged-column error messages.

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -162,12 +162,12 @@ if (run.integration.tests) {
             test_that("Trying to update with too many values fails", {
                 skip_on_jenkins("Silence the error emails")
                 expect_error(ds$v4[ds$v4 == "B"] <- rep(1, len + 5), 
-                    "expected 10 values, got 15")
+                    "Column length 15 does not match existing length 10.")
             })
             test_that("Trying to update with too few values fails", {
                 skip_on_jenkins("Silence the error emails")
                 expect_error(ds$v4[ds$v4 == "B"] <- rep(1, len - 3), 
-                    "expected 10 values, got 7")
+                    "Column length 7 does not match existing length 10.")
             })
             test_that("Trying to update with different filters fails", {
                 expect_error(ds$v4[ds$v4 == "B"] <- ds$v4[ds$v4 == "C"], 
