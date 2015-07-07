379ded04e94747d0a78b95ddc2976ebdbc1abe44
Neal Richardson <neal@crunch.io>
2015-07-07 12:49:05 -0700

    Document versioning functions

--- R/versions.R
+++ R/versions.R
@@ -7,6 +7,13 @@ init.VersionCatalog <- function (.Object, ...) {
 }
 setMethod("initialize", "VersionCatalog", init.VersionCatalog)
 
+##' Access the saved versions of a dataset
+##'
+##' @param x a \code{CrunchDataset}
+##' @return an object of class \code{VersionCatalog}. Supported methods on the
+##' catalog include "names" and "timestamps".
+##' @seealso \code{\link{saveVersion}} \code{\link{restoreVersion}}
+##' @export
 versions <- function (x) {
     stopifnot(is.dataset(x))
     return(VersionCatalog(crGET(shojiURL(x, "catalogs", "savepoints"))))
@@ -52,12 +59,29 @@ setMethod("show", "VersionCatalog", function (object) {
     invisible(out)
 })
 
+##' Create a new saved version
+##' 
+##' @param dataset a \code{CrunchDataset}
+##' @param description character name to give the saved version, as in a
+##' commit message. You are encouraged, though not strictly required, to give
+##' versions unique descriptions.
+##' @return invisibly, the URL of the newly created version
+##' @seealso \code{\link{versions}} \code{\link{restoreVersion}}
+##' @export
 saveVersion <- function (dataset, description=NULL) {
     u <- shojiURL(dataset, "catalogs", "savepoints")
     out <- crPOST(u, body=toJSON(list(description=description)))
     invisible(out)
 }
 
+##' Restore a dataset to a previously saved version
+##' 
+##' @param dataset a \code{CrunchDataset}
+##' @param version either the name ("description") of the version to restore to
+##' or the integer index of the version, as given by \code{versions(dataset)}
+##' @return \code{dataset}, rolled back to \code{version}.
+##' @seealso \code{\link{versions}} \code{\link{saveVersion}}
+##' @export
 restoreVersion <- function (dataset, version) {
     vcat <- versions(dataset)
     if (is.numeric(version)) {
