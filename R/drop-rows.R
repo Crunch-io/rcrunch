b7df2d76bcaf79547dd86d029a767e55a91d3700
Neal Richardson <neal@crunch.io>
2016-01-09 13:00:05 -0800

    Test and doc for dropRows

---
+++ R/drop-rows.R
@@ -0,0 +1,10 @@
+##' Permanently delete rows from a dataset
+##'
+##' @param dataset a \code{CrunchDataset}
+##' @param expr a \code{CrunchLogicalExpr}
+##' @return \code{dataset} without the rows indicated by \code{expr}
+##' @seealso \code{\link{exclusion}} for a non-destructive way to suppress rows
+##' @export
+dropRows <- function (dataset, expr) {
+
+}
