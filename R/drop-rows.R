fcea65a066c4caa53d65de89e5aa28e100374269
Neal Richardson <neal@crunch.io>
2016-01-11 10:25:10 -0800

    dropRows to delete rows from your dataset

--- R/drop-rows.R
+++ R/drop-rows.R
@@ -4,7 +4,16 @@
 ##' @param expr a \code{CrunchLogicalExpr}
 ##' @return \code{dataset} without the rows indicated by \code{expr}
 ##' @seealso \code{\link{exclusion}} for a non-destructive way to suppress rows
+##' @examples
+##' \dontrun{
+##' ds <- dropRows(ds, ds$gender == "Male")
+##'}
 ##' @export
 dropRows <- function (dataset, expr) {
-
+    ## Construct a ZCL delete payload, then POST it
+    payload <- list(command="delete",
+        filter=zcl(expr))
+    out <- crPOST(shojiURL(dataset, "fragments", "table"), body=toJSON(payload))
+    dropCache(self(dataset))
+    return(dataset)
 }
