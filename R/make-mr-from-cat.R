63dd199005feb27cf63d31fab70cdbc1e19d3217
Persephone Tsebelis <persephone@deliamac.local>
2016-01-27 13:53:24 -0800

    Make MR from categorical variables.

--- R/make-mr-from-cat.R
+++ R/make-mr-from-cat.R
@@ -1,22 +1,31 @@
-makeMRfromCat <- function(var1, var2, ...){
+##' Combine categorical variables into an MR
+##'
+##' This function lets you combine categorical variables into an MR variable
+##' Arguments include at least two categorical variables as well as any metadata
+##' for the new variable that is being created. The final argument useNAcats
+##' is given the defult value FALSE. If it is made TRUE, any categories that
+##' are NA in the categorical variables will become subvariables.
+
+makeMRfromCat <- function(var1, var2, ..., useNAcats = FALSE){
+    vars <- c(var1, var2, list(...)[sapply(1:length(list(...)), function(i) is.variable(list(...)[[i]]))])
     stopifnot(is.variable(var1) & is.variable(var2))
-    stopifnot(is.Categorical(var1) & is.Categorical(var2))
+    stopifnot(any(sapply(vars, is.Categorical)))
     
     newbody <- list()
-    oldbody1 <- updateList(copyVariableReferences(var1), tuple(var1)@body)
-    oldbody2 <- updateList(copyVariableReferences(var2), tuple(var2)@body)
-
-    newbody$name <- paste0(oldbody1$name, ", ", oldbody2$name, ", combine")
-    newbody$alias <- paste0(oldbody1$alias, "_", oldbody2$alias)
+    newbody$alias <- paste0(sapply(vars, alias), collapse = '_')
+    newbody$name <- paste0(paste0(sapply(vars, name), collapse = ', '), ", combine")
     newbody$type <- 'categorical_array'
-
-    newbody <- updateList(newbody, list(...))
     
-    var1.values <- as.vector(var1)
-    var2.values <- as.vector(var2)
-    newbody$subvariables <- lapply(union(names(categories(var1)), names(categories(var2))), function(subvar){
-        tmp <- factor(ifelse(var1.values %in% subvar | var2.values %in% subvar, 'selected', 'not selected'), levels=c('selected', 'not selected'))
-        VarDef(data=tmp,name=subvar, alias=paste0(newbody$alias, "_", gsub(" ", "_", tolower(subvar))))
+    newbody <- updateList(newbody, list(...)[!sapply(1:length(list(...)), function(i) is.variable(list(...)[[i]]))])
+
+    values <- matrix(sapply(vars, function(var) names(categories(var))[as.vector(var, mode='id')]), ncol=length(vars))
+    if (useNAcats) subs <- unique(unlist(lapply(vars, function(var) names(categories(var)))))
+    else subs <- unique(unlist(lapply(vars, function(var) names(categories(var))[!is.na(categories(var))])))
+
+    newbody$subvariables <- lapply(subs, function(sub){
+        VarDef(data=factor(ifelse(rowSums(values == sub,na.rm = TRUE) > 0, 'selected', 
+            ifelse(rowSums(is.na(values)) == length(vars), NA, 'not selected'))), alias=paste0(newbody$alias, "_", gsub(" ", "_", tolower(sub))),
+            name=sub)
     })
 
     class(newbody) <- 'VariableDefinition'
--- tests/testthat/test-mr-from-categorical.R
+++ tests/testthat/test-mr-from-categorical.R
@@ -1,19 +1,38 @@
-context('make MR from two categorical variables')
+context('make MR from categorical variables')
 
 if (run.integration.tests) {
     with(test.authentication, {
         with(test.dataset(newDatasetFromFixture("apidocs")), {
             expect_true('q1' %in% aliases(variables(ds)))
             expect_false('q2' %in% aliases(variables(ds)))
-            ds$q2 <- VariableDefinition(factor(rep(c('Lizard', 'Cat', "Don't know", 'Dog'), 5)), name='Pet 2', description='What is your second favorite pet?')
+            ds$q2 <- VariableDefinition(factor(rep(c('Lizard', 'Dog', "Don't know", 'Dog'), 5)), name='Pet 2', description='What is your second favorite pet?')
             expect_true('q2' %in% aliases(variables(ds)))
-            ds$q1_q2_test <- makeMRfromCat(ds$q1, ds$q2, name='test')
-            expect_true('q1_q2_test' %in% aliases(variables(ds)))
-            expect_false('q1_q2' %in% aliases(variables(ds)))
+            ds$q3_test <- VariableDefinition(factor(rep(c('Lizard', 'Dog', "Bee", 'Cat', 'Snake'), 4)), name='Pet 3', description='What is your third favorite pet?')
+            expect_true('q3_test' %in% aliases(variables(ds)))
+            expect_false(is.na(categories(ds$q3_test))[4])
+            is.na(categories(ds$q3_test))[names(categories(ds$q3_test)) %in% 'Cat'] <- TRUE
+            is.na(categories(ds$q3_test))[names(categories(ds$q3_test)) %in% 'Bee']  <- TRUE
+            ds$q1_q2_q3 <- makeMRfromCat(ds$q1, ds$q2, ds$q3_test, name='test')
+            expect_false('Bee' %in% names(subvariables(ds$q1_q2_q3)))
+            expect_true('Cat' %in% names(subvariables(ds$q1_q2_q3)))
             expect_true('test' %in% names(variables(ds)))
+            expect_true('q1_q2_q3' %in% aliases(variables(ds)))
+        })
+        with(test.dataset(newDatasetFromFixture("apidocs")), {
             expect_true('q1' %in% aliases(variables(ds)))
+            expect_false('q2' %in% aliases(variables(ds)))
+            ds$q2 <- VariableDefinition(factor(rep(c('Lizard', 'Dog', "Don't know", 'Dog'), 5)), name='Pet 2', description='What is your second favorite pet?')
             expect_true('q2' %in% aliases(variables(ds)))
-            expect_equal(names(subvariables(ds$q1_q2_test)), union(names(categories(ds$q1)), names(categories(ds$q2))))
+            ds$q3_test <- VariableDefinition(factor(rep(c('Lizard', 'Dog', "Bee", 'Cat', 'Snake'), 4)), name='Pet 3', description='What is your third favorite pet?')
+            expect_true('q3_test' %in% aliases(variables(ds)))
+            expect_false(is.na(categories(ds$q3_test))[4])
+            is.na(categories(ds$q3_test))[names(categories(ds$q3_test)) %in% 'Cat'] <- TRUE
+            is.na(categories(ds$q3_test))[names(categories(ds$q3_test)) %in% 'Bee']  <- TRUE
+            ds$q1_q2_q3 <- makeMRfromCat(ds$q1, ds$q2, ds$q3_test, name='test', useNAcats = TRUE)
+            expect_true('Bee' %in% names(subvariables(ds$q1_q2_q3)))
+            expect_true('Cat' %in% names(subvariables(ds$q1_q2_q3)))
+            expect_true('test' %in% names(variables(ds)))
+            expect_true('q1_q2_q3' %in% aliases(variables(ds)))
         })
     })
 }
