7f1e04417265477a416e7cb2fd0719eb5930867b
Neal Richardson <neal@crunch.io>
2016-02-16 11:47:10 -0800

    Doc and export forkDataset

--- R/fork-and-merge.R
+++ R/fork-and-merge.R
@@ -5,6 +5,17 @@ forks <- function (dataset) {
 
 setMethod("names", "ForkCatalog",  function (x) getIndexSlot(x, "name"))
 
+##' Create a fork of a dataset
+##'
+##' As with many other version control systems, in Crunch you can fork a
+##' dataset's revision history, effectively making a copy on which you can work
+##' independently of the original dataset. You can then merge those change back
+##' to the original dataset or keep working independently.
+##' @param dataset The CrunchDataset to fork
+##' @param forkname character name to give the fork. If omitted, one will be
+##' provided for you
+##' @return The new fork, a CrunchDataset.
+##' @export
 forkDataset <- function (dataset, forkname) {
     if (missing(forkname)) {
         nforks <- length(forks(dataset))
