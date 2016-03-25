cea8830e2ef117b0476a99239d2237aa807a52c3
Neal Richardson <neal@crunch.io>
2016-03-25 10:01:26 -0700

    Failing test

--- tests/testthat/helper-contexts.R
+++ tests/testthat/helper-contexts.R
@@ -2,7 +2,7 @@
 
 setup.and.teardown <- function (setup, teardown, obj.name=NULL) {
     ContextManager(enter=setup, exit=teardown, as=obj.name,
-        error=function (e) expect_error(stop(e$message), NA))
+        error=function (e) expect_error(stop(e$message), "NO ERRORS HERE!"))
 }
 
 with_mock_HTTP <- function (expr) {
