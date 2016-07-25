e6873326a1f0c44f84541ef99424361ec45aaa28
Neal Richardson <neal@crunch.io>
2016-07-25 11:06:43 -0700

    Successful extend operation, but arrays are not found

--- tests/testthat/test-merge-datasets.R
+++ tests/testthat/test-merge-datasets.R
@@ -27,12 +27,12 @@ with_mock_HTTP({
 
     test_that("Correct payload without filtering", {
         expect_warning(
-            expect_mock_request(merge(ds1, ds2, by.x=ds1$birthyr, ds2$birthyr),
-                'POST /api/datasets/dataset1/variables.json ',
+            expect_POST(merge(ds1, ds2, by.x=ds1$birthyr, ds2$birthyr),
+                '/api/datasets/dataset1/variables/',
                 '{"function":"adapt",',
-                '"args":[{"dataset":"/api/datasets/dataset3.json","filter":null},',
-                '{"variable":"/api/datasets/dataset3/variables/birthyr.json"},',
-                '{"variable":"/api/datasets/dataset1/variables/birthyr.json"}]}'),
+                '"args":[{"dataset":"/api/datasets/dataset3/","filter":null},',
+                '{"variable":"/api/datasets/dataset3/variables/birthyr/"},',
+                '{"variable":"/api/datasets/dataset1/variables/birthyr/"}]}'),
             "Variable birthyr is hidden")
     })
 })
@@ -56,34 +56,40 @@ printed_order_apidocs2 <- c(
     "Person ID"
 )
 
-with(temp.option(crunch.debug=TRUE, httpcache.log=""), {
-
 with_test_authentication({
-    with(test.dataset(newDatasetFromFixture("join-apidocs2-to-me"), "ds1"), {
-        ds1$allpets_1 <- NULL
-        with(test.dataset(newDatasetFromFixture("apidocs2"), "ds2"), {
-            test_that("Shape of apidocs2", {
-                expect_output(ordering(ds2),
-                    paste(printed_order_apidocs2, collapse="\n"))
-            })
-
-            test_that("An uncomplicated merge on a text key", {
-                ds1 <- merge(ds1, ds2, by.x=ds1$id, by.y=ds2$stringid)
-                expect_is(ds1, "CrunchDataset")
-                expect_identical(dim(ds1), c(14L, 13L))
-                expect_output(ordering(ds1),
-                    paste(
-                        "ID",
-                        "Join matches",
-                        "Another variable",
-                        paste0("[+] ", name(ds2)),
-                        paste("    ",
-                            printed_order_apidocs2[-length(printed_order_apidocs2)],
-                            sep="", collapse="\n")
-                        ), collapse="\n")
-            })
-        })
+    ds1 <- newDatasetFromFixture("join-apidocs2-to-me")
+    ds1$allpets_1 <- NULL
+    ds2 <- newDatasetFromFixture("apidocs2")
+    test_that("Shape of apidocs2", {
+        expect_output(ordering(ds2),
+            paste(printed_order_apidocs2, collapse="\n"), fixed=TRUE)
+        expect_identical(names(ds2), c("allpets", "q1", "petloc", "ndogs",
+            "ndogs_a", "ndogs_b", "q3", "country", "wave", "stringid"))
+        expect_identical(dim(ds2), c(20L, 10L))
     })
-})
+    test_that("Shape of join-to-me", {
+        expect_identical(names(ds1), c("id", "matches", "other_var"))
+        expect_identical(dim(ds1), c(14L, 3L))
+    })
 
+    test_that("An uncomplicated merge on a text key", {
+        ds1 <- merge(ds1, ds2, by.x=ds1$id, by.y=ds2$stringid)
+        expect_is(ds1, "CrunchDataset")
+        expect_identical(dim(ds1), c(14L, 12L))
+        expect_output(ordering(ds1),
+            paste(c(
+                "ID",
+                "Join matches",
+                "Another variable",
+                paste0("[+] ", name(ds2)),
+                paste("    ",
+                    printed_order_apidocs2[-length(printed_order_apidocs2)],
+                    sep="", collapse="\n")),
+                collapse="\n"),
+            fixed=TRUE)
+        expect_identical(names(ds1),
+            c("id", "matches", "other_var", "allpets", "q1", "petloc", "ndogs",
+            "ndogs_a", "ndogs_b", "q3", "country", "wave"))
+        expect_identical(hiddenVariables(ds1, "name"), c("Case ID", "Weight"))
+    })
 })
