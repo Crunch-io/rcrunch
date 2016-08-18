127a3551d0d07548274976f95cc9f4cfd65f7a80
Robert Brewer <fumanchu@aminus.org>
2016-08-18 12:29:37 -0700

    A couple error messages changed for real arrays.

--- tests/testthat/test-append-debug.R
+++ tests/testthat/test-append-debug.R
@@ -45,7 +45,7 @@ with_test_authentication({
             expect_error(
                 expect_message(appendDataset(part1, part2),
                     "Result URL"),
-                "common subvariables")
+                "Subvariable mr_1 cannot be bound to both arrays CA2 and CA1.")
         })
         part1 <- cleanseBatches(part1)
 
