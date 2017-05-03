0383d900bb804492aca9dc880254f02f3b509397
Neal Richardson <neal@crunch.io>
2017-05-02 17:16:30 -0700

    Doc

--- R/search.R
+++ R/search.R
@@ -5,6 +5,8 @@ searchDatasets <- function (query, ...) {
     return(SearchResults(results[["groups"]][[1]]))
 }
 
+#' @rdname dataset-variables
+#' @export
 setMethod("variables", "SearchResults", function (x) {
     ## Close enough to a catalog object
     VariableCatalog(structure(list(index=x$variables), class="shoji"))
