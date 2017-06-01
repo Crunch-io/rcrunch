60c2829c6a01a21865c1cd017161d1c3ce612a02
Jonathan Keane <jon@crunch.io>
2017-06-01 17:07:20 -0500

    importfrom utils instead of ::

--- R/github.R
+++ R/github.R
@@ -21,6 +21,7 @@ notifyIfNewVersion <- function (
 #' @param github.url character where to GET the tagged versions of the package
 #' @param installed.version character the currently installed version string
 #' @return The version string if there is a new version, or NULL
+#' @importFrom utils compareVersion
 #' @export
 #' @keywords internal
 checkForNewVersion <- function (
@@ -33,7 +34,7 @@ checkForNewVersion <- function (
         ## Filter to keep only those that match x.y.z
         version.tags <- grep("^[0-9]+\\.[0-9]+\\.[0-9]+$", gh.tags, value=TRUE)
         if (length(version.tags) &&
-            utils::compareVersion(version.tags[1], installed.version) == 1) {
+            compareVersion(version.tags[1], installed.version) == 1) {
             return(version.tags[1])
         }
     }
