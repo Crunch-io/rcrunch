ece632326a685311fa2900da7636c220630fe0fe
Jonathan Keane <jon@crunch.io>
2017-06-16 11:26:39 -0500

    Changed the makeCaseVariable interface to be more flexible.

--- R/case-variables.R
+++ R/case-variables.R
@@ -1,13 +1,43 @@
-#' @rdname describe
-#' @export
-setMethod("name", "Case", function (x) x@name)
-#' @rdname describe
-#' @export
-setMethod("name<-", "Case",
-          function (x, value) {
-              x@name <- validateNewName(value)
-              return(x)
-          })
+#' Case statement for case variables
+#'
+#' List elements: 
+#' id an integer to use for this category when a case variable is made 
+#' (default: none, one will automatically be assigned when the case variable 
+#' is made)
+#' name a charcater identifier for this case
+#' expression `CrunchLogicalExpr` which sets the conditions for the case
+#' numeric_value a numeric which is the value this case should take on 
+#' (useful when made into a case variable)
+#' missing a logical indicating if this case should be treated as missing
+#' 
+#' @keywords internal
+ensureValidCases <- function(case) {
+    if (!is.list(case)) {
+        halt("A case must be a list")
+    }
+    wrong_case_names <- setdiff(names(case), c("id", "name", "expression",
+                                               "numeric_value", "missing"))
+    if (length(wrong_case_names) > 0) {
+        halt("each case must have at most an id, name, expression, numeric_value, ", 
+             "and missing element")
+    }
+    if (!is.null(case$id) && !is.integer(case$id)) {
+        halt("a case's id must be an integer")
+    }
+    if (!is.character(case$name)) {
+        halt("a case's name must be a character and must not be NULL")
+    }
+    if (class(case$expression) != "CrunchLogicalExpr") {
+        halt("a case's expression must be a CrunchLogicalExpr and must not be NULL")
+    }
+    if (!is.null(case$numeric_value) && !is.numeric(case$numeric_value)) {
+        halt("a case's numeric_value must be a numeric")
+    }
+    if (!is.null(case$missing) && !is.logical(case$missing)) {
+        halt("a case's missing must be a logical")
+    }
+    return(case)
+}
 
 #' Make a case variable
 #' 
@@ -15,42 +45,104 @@ setMethod("name<-", "Case",
 #' variables. These are evaluated in the order they are supplied in the list 
 #' as the `cases` argument (they proceed in an IF, ELSE IF, ELSE IF, ..., ELSE 
 #' fashion); the first one that matches selects the corresponding value from 
-#' the [`Case`] object.
+#' the case list.
 #' 
-#' @param cases A list of objects of class [`Case`]
-#' @param ... other properties to pass about the case variable (e.g. name)
+#' There are two ways to specify cases, but you must pick only one (note these 
+#' two will produce the same case variable):
+#' 
+#' 1. When you just want to specify conditions, you can use named conditions:
+#' `makeCaseVaraible(case1=ds$v1 == 1, case2=ds$v2 == 2, name="new case")`
+#' 
+#' 1. You can also use the `cases` argument, which is useful when you want to 
+#' prespecify ids, or set numeric_values:
+#' `makeCaseVaraible(cases=list(list(expression=ds$v1 == 1, name="case1"), list(expression=ds$v2 == 2, name="case2")), name="new case")`
+#' 
+#' @param ... a sequence of named expressions to use as cases as well as other 
+#' properties to pass about the case variable (i.e. alias, description)
+#' @param cases a list of lists with each case condition to use each must 
+#' include at least a `name` and an `expression` element.
+#' @param else_case a single list that has at least a `name` to serve as the 
+#' case when no others match (if not specified Crunch will use the system 
+#' default "No data")
+#' @param name a character to use as the name of the case variable to create
 #' @export
-makeCaseVariable <- function (cases, ...) {
-    # check if all cases are cases.
-    if (any(sapply(cases, class) != "Case")) {
-        halt("All elements of the cases arugment must be of class Case")
+makeCaseVariable <- function (..., cases, else_case, name) {
+    dots <- list(...)
+    is_expr <- function (x) inherits(x, "CrunchLogicalExpr")
+    exprs <- Filter(is_expr, dots)
+    if (!missing(cases) & length(exprs) > 0) {
+        halt("can't have case conditions both in ... as well as in the cases ",
+             "argument, please use one or the other.")
     }
-
-    # make ids if cases don't have ids.
-    need_ids <- sapply(cases, function(x) length(x@id) == 0)
+    if (missing(cases) & length(exprs) == 0) {
+        halt("must supply case conditions in either ... or the cases ",
+             "argument, please use one or the other.")
+    }
+    if (missing(cases) & length(exprs) > 0) {
+        # save any member of dots that isn't an expression for use later.
+        dots <- Filter(Negate(is_expr), dots)
+        cases <- mapply(function (expr, name) list(name=name, expression=expr),
+                        expr=exprs, name=names(exprs),
+                        SIMPLIFY = FALSE, USE.NAMES = FALSE)
+    }
+    
+    # make ids if the cases don't have ids.
+    need_ids <- sapply(cases, function(x) is.null(x$id))
     cases[need_ids] <- lapply(seq_along(cases[need_ids]), function(i) {
-        cases[need_ids][[i]]@id <- i
+        cases[need_ids][[i]]$id <- i
         return(cases[need_ids][[i]])
     })
+    
+    # NULLify numeric_value and FALSE missing if they are empty
+    cases <- lapply(cases, function(x) {
+        if(is.null(x$numeric_value)) {
+            x['numeric_value'] <- list(NULL)
+        }
+        if(is.null(x$missing)) {
+            x['missing'] <- FALSE
+        }
+        return(x)
+    })
+    cases <- lapply(cases, ensureValidCases)
 
+    if (!missing(else_case)) {
+        # assign API default values if not given
+        if(is.null(else_case$id)) {
+            else_case['id'] <- -1L
+        }
+        if(is.null(else_case$numeric_value)) {
+            else_case['numeric_value'] <- list(NULL)
+        }
+        if(is.null(else_case$missing)) {
+            else_case['missing'] <- FALSE
+        }
+        if(!is.null(else_case$expression)) {
+            halt("else_cases should not have any conditions expression")
+        }
+        if(!is.character(else_case$name)) {
+            halt("else_cases must have a (character) name")
+        }
+        if(!is.integer(else_case$id)) {
+            halt("id must be an integer")
+        }
+        cases <- append(cases, list(else_case))
+    }
+    
     # create the new categorical variable
     new_cat_type <- list(
         value=list(class="categorical",
-        categories=lapply(cases, function (case) {
-            # if the numeric_value is empty, then use null
-            num_value <- switch(length(case@numeric_value)>0,case@numeric_value)
-            list(id=case@id, name=name(case),
-                 numeric_value=num_value,
-                 missing=case@missing)
-        })))
+        categories=lapply(cases, function (case) case[c("id", "name", "numeric_value", "missing")])))
     new_cat_ids <- sapply(new_cat_type$value$categories, function(x) x$id)
     new_cat <- list(column=I(new_cat_ids), type=new_cat_type)
     
-    casevar <- list(...)
-    casevar$derivation <- zfunc("case", c(list(new_cat), lapply(cases, zcl)))
+    casevar <- dots
+    casevar$name <- name
+    casevar$derivation <- zfunc("case", new_cat)
 
-    # remove a level of duplicate embedding
-    casevar$derivation$args <- casevar$derivation$args[[1]]
+    # add case_expressions, remove nulls (should only be from the else case)
+    case_exprs <- lapply(cases, function(x) zcl(x$expression))
+    case_exprs <- Filter(Negate(is.null), case_exprs)
+    casevar$derivation$args <- append(casevar$derivation$args, case_exprs)
 
     class(casevar) <- "VariableDefinition"
     return(casevar)
--- tests/testthat/test-case-variable.R
+++ tests/testthat/test-case-variable.R
@@ -3,11 +3,22 @@ context("Case variables")
 with_mock_crunch({
     ds <- loadDataset("test ds")
     
-    test_that("Case class", {
-        case <- Case(name="Dudes", case=ds$gender == "Male")
-        expect_equal(name(case), "Dudes")
-        name(case) <- "Cool Dudes"
-        expect_equal(name(case), "Cool Dudes")
+    test_that("Case list validator", {
+        case <- list(name="Dudes", expression=ds$gender == "Male")
+        expect_equal(ensureValidCases(case), case)
+        expect_error(ensureValidCases("case"), "A case must be a list")
+        expect_error(ensureValidCases(list()),
+                     "a case's name must be a character and must not be NULL")
+        expect_error(ensureValidCases(list(name="name")),
+                     "a case's expression must be a CrunchLogicalExpr and must not be NULL")
+        expect_error(ensureValidCases(list(name="name", expression=CrunchLogicalExpr(), id=0.8)),
+                     "a case's id must be an integer")
+        expect_error(ensureValidCases(list(name="name", expression=CrunchLogicalExpr(), numeric_value="nope")),
+                     "a case's numeric_value must be a numeric")
+        expect_error(ensureValidCases(list(name="name", expression=CrunchLogicalExpr(), missing="nope")),
+                     "a case's missing must be a logical")
+        expect_error(ensureValidCases(list(not_right="not")),
+                     "each case must have at most an id, name, expression, numeric_value, and missing element")
     })
 
     case_output <- list(
@@ -46,13 +57,21 @@ with_mock_crunch({
     test_that("Case variable definition", {
         expect_json_equivalent(
             makeCaseVariable(
-                list(
-                    Case(case=ds$gender == "Male", name="Dudes"),
-                    Case(case=ds$birthyr < 1950, name="Old women")
+                cases = list(
+                    list(expression=ds$gender == "Male", name="Dudes"),
+                    list(expression=ds$birthyr < 1950, name="Old women")
                 ),
                 name="Super clever segmentation"),
             case_output)
-        
+    })
+    test_that("makeCaseVariable works with ... specification", {
+        expect_json_equivalent(
+            makeCaseVariable(`Dudes`=ds$gender == "Male",
+                             `Old women`=ds$birthyr < 1950,
+                             name="Super clever segmentation"),
+            case_output)
+    })
+    test_that("makeCaseVariable works with ids pre-specified", {
         case_output$derivation$args[[1]]$column <- I(10:11)
         case_output$derivation$args[[1]]$type$value$categories[[1]]$id <- 10L
         case_output$derivation$args[[1]]$type$value$categories[[1]]$numeric_value <- 0
@@ -60,31 +79,75 @@ with_mock_crunch({
         case_output$derivation$args[[1]]$type$value$categories[[2]]$missing <- TRUE
         expect_json_equivalent(
             makeCaseVariable(
-                list(
-                    Case(id=10L, case=ds$gender == "Male", name="Dudes", numeric_value=0),
-                    Case(id=11L, case=ds$birthyr < 1950, name="Old women", missing=TRUE)
+                cases = list(
+                    list(id=10L, expression=ds$gender == "Male", name="Dudes", numeric_value=0),
+                    list(id=11L, expression=ds$birthyr < 1950, name="Old women", missing=TRUE)
                 ),
+                name="Super clever segmentation"),
+            case_output)
+    })
+    test_that("makeCaseVariable works with an else pre-specified", {
+        case_output$derivation$args[[1]]$column[[3]] <- 3L
+        case_output$derivation$args[[1]]$type$value$categories[[3]] <- list(
+            id=3L, name="Other", numeric_value=NULL, missing=FALSE)
+        expect_json_equivalent(
+            makeCaseVariable(
+                cases = list(
+                    list(expression=ds$gender == "Male", name="Dudes"),
+                    list(expression=ds$birthyr < 1950, name="Old women")
+                ),
+                else_case = list(name="Other", id=3L),
                 name="Super clever segmentation"),
             case_output)
     })
     
     test_that("makeCaseVariable errors gracefully", {
-        expect_error(makeCaseVariable(cases = list(
-            Case(case=ds$gender == "Male", name="Dudes"),
-            "not a Case object"), name = "foo"),
-            "All elements of the cases arugment must be of class Case")
+        expect_error(makeCaseVariable(cases=list(
+            list(expression=ds$gender == "Male", name="Dudes"))),
+            'argument "name" is missing, with no default')
+        expect_error(makeCaseVariable(`Old women`=ds$birthyr < 1950, cases=list(
+            list(expression=ds$gender == "Male", name="Dudes"))),
+            "can't have case conditions both in ... as well as in the cases argument, please use one or the other.")
+        expect_error(makeCaseVariable(name="Dudes"),
+            "must supply case conditions in either ... or the cases argument, please use one or the other.")
+        expect_error(makeCaseVariable(cases=list(
+            list(expression=ds$gender == "Male", name="Dudes")),
+            else_case = list(expression=ds$gender == "Female", name="Female")),
+            "else_cases should not have any conditions expression")
+        expect_error(makeCaseVariable(cases=list(
+            list(expression=ds$gender == "Male", name="Dudes")),
+            else_case = list(id=1L)),
+            "else_cases must have a \\(character\\) name")
+        expect_error(makeCaseVariable(cases=list(
+            list(expression=ds$gender == "Male", name="Dudes")),
+            else_case = list(name="name", id=0.8)),
+            "id must be an integer")
     })
 })
 
 with_test_authentication({
     ds <- newDatasetFromFixture("apidocs")
-    ds$catdog <- makeCaseVariable(list(Case(case = ds$q1 == "Cat", name="Cats"),
-                                       Case(case = ds$q1 == "Dog", name="Dogs")),
-                                  name = "Cats or Dogs")
-    expect_equal(as.vector(ds$catdog)[1:10],
-                 factor(c(NA, "Cats", NA, "Dogs", "Dogs", NA, NA, NA,
-                          "Cats", "Dogs"), levels = (c("Cats", "Dogs"))))
-    expect_equal(name(ds$catdog), "Cats or Dogs")
-    expect_equal(ids(categories(ds$catdog)), c(1,2,-1))
-    expect_equal(names(categories(ds$catdog)), c("Cats", "Dogs", "No Data"))
+    test_that("makeCaseVariables", {
+        ds$catdog <- makeCaseVariable(`Cats`=ds$q1 == "Cat",
+                                      `Dogs`=ds$q1 == "Dog",
+                                      name="Cats or Dogs",
+                                      description = "Describe cats and dogs")
+        expect_equal(as.vector(ds$catdog)[1:10],
+                     factor(c(NA, "Cats", NA, "Dogs", "Dogs", NA, NA, NA,
+                              "Cats", "Dogs"), levels=(c("Cats", "Dogs"))))
+        expect_equal(name(ds$catdog), "Cats or Dogs")
+        expect_equal(description(ds$catdog), "Describe cats and dogs")
+        expect_equal(ids(categories(ds$catdog)), c(1,2,-1))
+        expect_equal(names(categories(ds$catdog)), c("Cats", "Dogs", "No Data"))
+        
+        ds$catdog2 <- makeCaseVariable(cases=list(list(expression=ds$q1 == "Cat", name="Cats"),
+                                                 list(expression=ds$q1 == "Dog", name="Dogs")),
+                                       else_case=list(id=99L, name="Other", missing=FALSE),
+                                      name="Cats or Dogs2")
+        expect_equal(as.vector(ds$catdog2)[1:10],
+                     factor(c("Other", "Cats", "Other", "Dogs", "Dogs", "Other", "Other", "Other",
+                              "Cats", "Dogs"), levels=(c("Cats", "Dogs", "Other"))))
+        expect_equal(ids(categories(ds$catdog2)), c(1,2,99))
+        expect_equal(names(categories(ds$catdog2)), c("Cats", "Dogs", "Other"))
+    })
 })
