a8b84c705ee409d2d7ee9fd3b855225bc2b4cbb1
Jonathan Keane <jkeane@gmail.com>
2017-07-03 17:14:24 -0500

    Additional tests

--- tests/testthat/test-conditional-transform.R
+++ tests/testthat/test-conditional-transform.R
@@ -2,6 +2,11 @@ context("Conditional transform")
 
 with_mock_crunch({
     ds <- loadDataset("test ds")
+    test_that("conditionalTransform", {
+    expect_error(conditionalTransform(cases=list(ds$girthyr < 1945), values=list("foo", "bar")),
+                 dQuote("cases"), " and ", dQuote("values"), " must be the same length")
+
+    })
 })
 
 with_test_authentication({
@@ -12,5 +17,11 @@ with_test_authentication({
         expect_equal(out, c("Jasmine", NA, "2", "3", "Zeus", "2", "2", "3",
                             "2", "2", "2", NA, "3", "Belgium", "6", "Fluffy",
                             NA, "Austria", NA, "2"))
+
+        out <- conditionalTransform(cases = list(ds$ndogs < 1, ds$ndogs == 1, ds$ndogs > 1),
+                                    values=list("lonely", ds$q3, ds$ndogs))
+        expect_equal(out, c("Jasmine", NA, "2", "3", "Zeus", "2", "2", "3",
+                            "2", "2", "2", NA, "3", "lonely", "6", "Fluffy",
+                            NA, "lonely", NA, "2"))
     })
 })
