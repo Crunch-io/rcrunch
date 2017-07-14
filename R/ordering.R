78a0f09ed59d4dcfd8cf475897422950441f8e5d
Persephone Tsebelis <persephone.tsebelis@yougov.com>
2017-07-14 15:52:25 +0100

    adding copyOrder and moveToAfter

--- R/ordering.R
+++ R/ordering.R
@@ -82,3 +82,38 @@ setMethod("ordering<-", "DatasetCatalog", function (x, value) {
     }
     return(x)
 })
+
+
+moveToAfter <- function(x, value, after){
+    if (!inherits(after, "OrderGroup")) {
+        after <- urls(after)
+    }
+    if (!inherits(value, "OrderGroup")) {
+        value <- urls(value)
+    }
+    whi <- ifelse(inherits(after, "OrderGroup"), which(names(x) %in% name(after)), which(entities(x) %in% after))
+    if ((inherits(value, "OrderGroup") && !name(value) %in% names(x)) || (!inherits(value, "OrderGroup") && !value %in% entities(x))) entities(x) <- c(entities(x), value)
+    whi2 <- ifelse(inherits(value, "OrderGroup"), which(names(x) %in% name(value)), which(entities(x) %in% value))
+    entities(x) <- entities(x)[c(setdiff(1:whi, whi2), whi2, setdiff((whi+1):length(entities(x)), whi2))]
+    return(x)
+}
+
+
+copyOrder <- function(ds_old, ds_new, ord_old=NULL){
+    if (is.null(ord_old)) ord_old <- ordering(ds_old)
+    ents <- c()
+    print(name(ord_old))
+    if (class(unlist(entities(ord_old))) == 'character') {
+        als <- aliases(allVariables(ds_old))[match(unlist(entities(ord_old)), urls(allVariables(ds_old)))]
+        ents <- urls(allVariables(ds_new))[match(als[als %in% aliases(allVariables(ds_new))], aliases(allVariables(ds_new)))]
+    } else{
+        for (gr in entities(ord_old)){
+            if (class(gr) == 'character'){
+                al <- aliases(allVariables(ds_old))[match(gr, urls(allVariables(ds_old)))]
+                if (al %in% aliases(allVariables(ds_new))) ents <- c(ents, self(ds_new[[al]]))
+            } else ents <- c(ents, steal_order(ds_old, ds_new, gr))
+        }
+    }
+    if (length(ents) > 0 & !is.null(name(ord_old))) return(VariableGroup(name(ord_old), ents))
+    if (length(ents) > 0) return(ents)
+}
