a2069be456d590f51ede8cbddb8d34cd4f06f3fd
Neal Richardson <neal@crunch.io>
2017-08-29 10:13:05 -0700

    Build pkgdown site on travis only on tagged versions

--- pkgdown.sh
+++ pkgdown.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
-    [ "${TRAVIS_TAG}" = "" ] &&
+    [ "${TRAVIS_TAG}" != "" ] &&
     [ "${TRAVIS_R_VERSION_STRING}" = "release" ]; then
 
-    ## Use TRAVIS_TAG != "" instead
     R CMD INSTALL . ## Package needs to be installed, apparently
     R -e 'devtools::install_github("hadley/pkgdown"); pkgdown::build_site()'
     git clone --branch src https://${GH_TOKEN}@github.com/Crunch-io/ta-da.git ../ta-da
