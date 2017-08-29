29eda489eaf36894185a651c51c4d42c71139f89
Neal Richardson <neal@crunch.io>
2017-08-28 19:21:20 -0700

    Case sensitivity

--- pkgdown.sh
+++ pkgdown.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
-    [ "${TRAVIS_BRANCH}" = "pkgdown" ] &&
+    [ "${TRAVIS_TAG}" = "" ] &&
     [ "${TRAVIS_R_VERSION_STRING}" = "release" ]; then
 
     ## Use TRAVIS_TAG != "" instead
     R CMD INSTALL . ## Package needs to be installed, apparently
     R -e 'devtools::install_github("hadley/pkgdown"); pkgdown::build_site()'
     git clone --branch src https://${GH_TOKEN}@github.com/Crunch-io/ta-da.git ../ta-da
-    rm -rf ../ta-da/static/R/crunch
-    cp -r docs/. ../ta-da/static/R/crunch
+    rm -rf ../ta-da/static/r/crunch
+    cp -r docs/. ../ta-da/static/r/crunch
     cd ../ta-da
     git add .
     git commit -m "Updating rcrunch pkgdown site (rcrunch build ${TRAVIS_BUILD_NUMBER})" || true
