498bf7e73a74ece63908906a04ace2ec16753fb8
Jonathan Keane <jon@crunch.io>
2017-09-08 16:43:23 +0100

    Cleaned up conditional transform

--- R/conditional-transform.R
+++ R/conditional-transform.R
@@ -1,53 +1,52 @@
 #' Conditional transformation
 #'
-#' Create a new variable that has values when specific conditions are met. 
-#' Conditions are specified using a series of formulas: the righthand side is 
-#' the condition that must be true (a `CrunchLogicalExpr`) and the lefthand 
-#' side is where to get the value if the condition on the righthand side is 
-#' true. This is commonly a Crunch variable but may be a string or numeric 
+#' Create a new variable that has values when specific conditions are met.
+#' Conditions are specified using a series of formulas: the righthand side is
+#' the condition that must be true (a `CrunchLogicalExpr`) and the lefthand
+#' side is where to get the value if the condition on the righthand side is
+#' true. This is commonly a Crunch variable but may be a string or numeric
 #' value, depending on the type of variable you're constructing.
 #'
-#' `conditionalTransform` is similar to `makeCaseVariable`; however, 
-#' `conditionalTransform` can use other Crunch variables as a source of a 
-#' variable, whereas, `makeCaseVariable` can only use characters. This 
-#' additional power comes at a cost: `makeCaseVariable` can be executed 
-#' entirely on Crunch servers, so no data needs to be downloaded or uploaded 
-#' to/from the local R session. `conditionalTransform` on the other hand will 
+#' `conditionalTransform` is similar to `makeCaseVariable`; however,
+#' `conditionalTransform` can use other Crunch variables as a source of a
+#' variable, whereas, `makeCaseVariable` can only use characters. This
+#' additional power comes at a cost: `makeCaseVariable` can be executed
+#' entirely on Crunch servers, so no data needs to be downloaded or uploaded
+#' to/from the local R session. `conditionalTransform` on the other hand will
 #' download the data necessary to construct the new variable.
-#' 
+#'
 #' For more details see the \code{vignette("conditional-variables", package="crunch")} vignette.
 #'
 #' @param ... a list of cases to evaluate as well as other
 #' properties to pass about the case variable (i.e. alias, description)
 #' @param data a Crunch dataset object to use
-#' @param else_condition a default value to use if none of the conditions are 
+#' @param else_condition a default value to use if none of the conditions are
 #' true (default: `NA`)
 #' @param type a character that is either "categorical", "text", "numeric" what
-#'  type of output should be returned? The source variables will be converted 
+#'  type of output should be returned? The source variables will be converted
 #'  to this type if necessary
-#' @param categories a vector of characters if `type="categorical"`, these are 
+#' @param categories a vector of characters if `type="categorical"`, these are
 #' all of the categories that should be in the resulting variable, in the order
 #' they should be in the resulting variable or a set of Crunch categories.
 #'
 #' @return a Crunch `VariableDefinition`
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' ds$cat_opinion <- conditionalTransform(pet1 == 'Cat' ~ Opinion1,
 #'                                        pet2 == 'Cat' ~ Opinion2,
-#'                                        pet3 == 'Cat' ~ Opinion3, 
+#'                                        pet3 == 'Cat' ~ Opinion3,
 #'                                        data = ds,
-#'                                        name = "Opinoin of Cats")
+#'                                        name = "Opinion of Cats")
 #' }
 #' @export
 conditionalTransform <- function (..., data, else_condition=NA, type="categorical", categories=NULL) {
     dots <- list(...)
     is_formula <- function (x) inherits(x, "formula")
-    formulas <- Filter(is_formula, dots) 
+    formulas <- Filter(is_formula, dots)
     var_def <- Filter(Negate(is_formula), dots)
-    n <- length(formulas)
-    
-    if (n == 0) {
+
+    if (length(formulas) == 0) {
         halt("no conditions have been supplied; please supply formulas as conditions.")
     }
     if (!type %in% c("categorical", "text", "numeric")){
@@ -56,70 +55,22 @@ conditionalTransform <- function (..., data, else_condition=NA, type="categorica
     if (type != "categorical" & !is.null(categories)){
         halt("type is not ", dQuote("categorical"), " ignoring ", dQuote("categories"))
     }
-    
-    cases <- vector("list", n)
-    values <- vector("list", n)
-    for (i in seq_len(n)) {
-        formula <- formulas[[i]]
-        if (!inherits(formula, "formula") || length(formula) != 3) {
-            halt("The case provided is not a formula: ", deparseAndFlatten(formula))
-        }
-        
-        cases[[i]] <- evalLHS(formula, data)
-        if (!inherits(cases[[i]], "CrunchLogicalExpr")) {
-            halt("The LHS provided is not a CrunchLogicalExpr: ", formulaLHS(formula))
-        }
-        values[[i]] <- evalRHS(formula, data)
-    }
-    case_indices <- lapply(cases, which)
-    
-    # dedpulicate indices, favoring the first true condition
-    case_indices <- lapply(seq_along(case_indices), function(i) {
-        setdiff(case_indices[[i]], unlist(case_indices[seq_len(i-1)]))
-    })
-    
-    # grab the values needed from source variables
-    values_to_fill <- Map(function(ind, var) {
-        if (inherits(var, c("CrunchVariable", "CrunchExpr"))) {
-            # grab the variable contents at inds
-            return(as.vector(var[ind]))
-        } else {
-            # if var isn't a crunch variable or expression, just return var
-            return(var)
-        }
-    }, ind = case_indices, var = values)
-    
-    # setup NAs for as default
-    n_rows <- nrow(CrunchDataset(crGET(datasetReference(cases[[1]]))))
-    result <- rep(else_condition, n_rows)
-    
-    # fill values
-    for (i in seq_along(case_indices)) {
-        if (type == "numeric") {
-            trans <- as.numeric
-        } else {
-            trans <- as.character
-        }
-        vals <- trans(values_to_fill[[i]])
-        result[case_indices[[i]]] <- vals
-    }
- 
+
+    result <- make_conditional_values(formulas, data, else_condition, type)
+
+    var_def$type <- type
+
     if (type == "categorical") {
         # if categories are supplied and there are any
         if (missing(categories)) {
             result <- factor(result)
-            # if categories aren't a Categories object,
             # make categories from names
             categories <- Categories(data = categoriesFromLevels(levels(result)))
-            categories <- c(categories, Category(data=.no.data))
         } else {
             if (!is.categories(categories)) {
                 # if categories aren't a Categories object,
                 # make categories from names
                 categories <- Categories(data = categoriesFromLevels(categories))
-                categories <- c(categories, Category(data=.no.data))
-            } else if (is.null(categories[["No data"]])) {
-                categories <- c(categories, Category(data=.no.data))
             }
 
             uni_results <- unique(result[!is.na(result)])
@@ -131,10 +82,7 @@ conditionalTransform <- function (..., data, else_condition=NA, type="categorica
             }
             result <- factor(result, levels = names(categories))
         }
-    }
-
-    var_def$type <- type
-    if (type == "categorical") {
+        categories <- ensureNoDataCategory(categories)
         # make a category list to send with VariableDefinition and then store that and convert values to ids values
         category_list <- listifyCategories(categories)
         var_def$categories <- category_list
@@ -142,9 +90,75 @@ conditionalTransform <- function (..., data, else_condition=NA, type="categorica
         vals[is.na(vals)] <- "No Data" # na is system default
         var_def$values <- ids(categories[vals])
     } else {
-        var_def$values <- result 
+        var_def$values <- result
     }
-    
+
     class(var_def) <- "VariableDefinition"
     return(var_def)
+}
+
+make_conditional_values <- function (formulas, data, else_condition, type) {
+    n <- length(formulas)
+    cases <- vector("list", n)
+    values <- vector("list", n)
+    for (i in seq_len(n)) {
+        formula <- formulas[[i]]
+        if (length(formula) != 3) {
+            halt("The case provided is not a proper formula: ", deparseAndFlatten(formula))
+        }
+
+        cases[[i]] <- evalLHS(formula, data)
+        if (!inherits(cases[[i]], "CrunchLogicalExpr")) {
+            halt("The LHS provided is not a CrunchLogicalExpr: ", LHS_string(formula))
+        }
+        values[[i]] <- evalRHS(formula, data)
+    }
+
+    # setup NAs for as default
+    if (!missing(data)) {
+        n_rows <- nrow(data)
+    } else {
+        # if there's no data argument, check all datasets are the same and get
+        # the reference from the unique one
+        ds_refs <- unlist(unique(lapply(cases, datasetReference)))
+        if (length(ds_refs) > 1) {
+            halt("There is more than one dataset referenced. Please ",
+                 "supply only one.")
+        }
+        n_rows <- nrow(CrunchDataset(crGET(ds_refs)))
+    }
+
+    # grab booleans for cases
+    case_indices <- lapply(cases, which)
+
+    # deduplicate indices, favoring the first true condition
+    case_indices <- lapply(seq_along(case_indices), function(i) {
+        setdiff(case_indices[[i]], unlist(case_indices[seq_len(i-1)]))
+    })
+
+    if (type == "numeric") {
+        trans <- as.numeric
+    } else {
+        trans <- as.character
+    }
+    # grab the values needed from source variables
+    values_to_fill <- Map(function(ind, var) {
+        if (inherits(var, c("CrunchVariable", "CrunchExpr"))) {
+            # grab the variable contents at inds
+            return(trans(as.vector(var[ind])))
+        } else {
+            # if var isn't a crunch variable or expression, just return var
+            return(trans(var))
+        }
+    }, ind = case_indices, var = values)
+
+    result <- rep(else_condition, n_rows)
+
+    # fill values
+    for (i in seq_along(case_indices)) {
+        vals <- values_to_fill[[i]]
+        result[case_indices[[i]]] <- vals
+    }
+
+    return(result)
 }
--- tests/testthat/test-conditional-transform.R
+++ tests/testthat/test-conditional-transform.R
@@ -2,6 +2,8 @@ context("Conditional transform")
 
 with_mock_crunch({
     ds <- loadDataset("test ds")
+    ds2 <- loadDataset("ECON.sav")
+
     test_that("conditionalTransform input validation", {
         expect_error(conditionalTransform("gender", data = ds),
                      'no conditions have been supplied; please supply formulas as conditions.')
@@ -13,28 +15,34 @@ with_mock_crunch({
                                           categories=c("foo", "bar")),
                      paste0("type is not ", dQuote("categorical"), " ignoring ",
                      dQuote("categories")))
+        # check that we can't reference two different datasets
+        expect_error(conditionalTransform(ds$gender == 'Male' ~ "foo",
+                                          ds2$gender == 'Male' ~ "foo",
+                                          name = "new"),
+                     paste0("There is more than one dataset referenced. Please ",
+                            "supply only one."))
     })
-    
+
     test_that("conditionalTransform works with categories", {
         expect_silent(new_var <- conditionalTransform(gender == "Male" ~ textVar, data = ds))
         expect_equal(new_var$values, c(-1, -1, -1, -1, -1, -1, 2, -1, -1, -1,
                                        3, -1, 4, -1, -1, -1, -1, -1, -1, -1, 1,
                                        6, 3, -1, 5))
         expect_equal(new_var$type, "categorical")
-        
+
         expect_silent(new_var <- conditionalTransform(ds$gender == "Male" ~ ds$textVar))
         expect_equal(new_var$values, c(-1, -1, -1, -1, -1, -1, 2, -1, -1, -1,
                                        3, -1, 4, -1, -1, -1, -1, -1, -1, -1, 1,
                                        6, 3, -1, 5))
         expect_equal(new_var$type, "categorical")
-        
+
         expect_error(conditionalTransform(gender == "Male" ~ textVar,
-                                          data = ds,categories = c("l", "m", 
-                                                                   "s", "h", 
+                                          data = ds,categories = c("l", "m",
+                                                                   "s", "h",
                                                                    "z")),
                      "there were categories in the results \\(x\\) that were not specified in categories")
     })
-        
+
     test_that("conditionalTransform works when specifying a categories as strings", {
         expect_silent(new_var <- conditionalTransform(gender == "Male" ~ textVar, data = ds,
                                         categories = c("l", "m", "s", "h", "z", "x")))
@@ -43,7 +51,7 @@ with_mock_crunch({
                                        5, 2, -1, 6))
         expect_equal(new_var$type, "categorical")
     })
-        
+
     test_that("conditionalTransform works when specifying a categories object", {
         # use different numeric values and missingnesses to check that the categories object is being sent
         textVarCatslist <- list(
@@ -63,7 +71,7 @@ with_mock_crunch({
                                        5, 2, -1, 6))
         expect_equal(new_var$type, "categorical")
         expect_json_equivalent(new_var$categories, c(textVarCatslist, list(no_data_cat)))
-        
+
         # reverse the ids to make sure that the ids are not being over-written
         textVarCatslist <- list(
             list(id=6L, name="l", numeric_value=10L, missing=FALSE),
@@ -85,7 +93,7 @@ with_mock_crunch({
         expect_equal(new_var$type, "categorical")
         expect_json_equivalent(new_var$categories, c(textVarCatslist, list(no_data_cat)))
     })
-        
+
     test_that("conditionalTransform works with other output types (text and numeric)", {
         expect_silent(new_var <- conditionalTransform(gender == "Male" ~ textVar, data = ds,
                                         type = "text"))
@@ -93,14 +101,14 @@ with_mock_crunch({
                                 NA, "s", NA, NA, NA, NA, NA, NA, NA, "h", "z",
                                 "m", NA, "x"))
         expect_equal(new_var$type, "text")
-        
+
         expect_silent(new_var <- conditionalTransform(gender == "Male" ~ "guy", data = ds,
                                         type = "text"))
         expect_equal(new_var$values, c(NA, NA, NA, NA, NA, NA, "guy", NA, NA, NA, "guy",
                                        NA, "guy", NA, NA, NA, NA, NA, NA, NA, "guy", "guy",
                                        "guy", NA, "guy"))
         expect_equal(new_var$type, "text")
-        
+
         expect_silent(new_var <- conditionalTransform(gender == "Male" ~ 1, data = ds,
                                         type = "numeric"))
         expect_equal(new_var$values, c(NA, NA, NA, NA, NA, NA, 1, NA, NA, NA, 1,
@@ -115,7 +123,7 @@ with_test_authentication({
     test_that("conditionalTransform", {
         ds$new0 <- conditionalTransform(ndogs < 1 ~ country,
                                     ndogs == 1 ~ q3,
-                                    ndogs > 1 ~ ndogs, 
+                                    ndogs > 1 ~ ndogs,
                                     data = ds)
         expect_equal(as.vector(ds$new0), factor(c("Jasmine", NA, "2", "3", "Zeus",
                                               "2", "2", "3", "2", "2", "2", NA,
@@ -125,7 +133,7 @@ with_test_authentication({
     test_that("conditionalTransform with else_condition", {
         ds$new1 <- conditionalTransform(ndogs < 1 ~ country,
                                     ndogs == 1 ~ q3,
-                                    ndogs > 1 ~ ndogs, 
+                                    ndogs > 1 ~ ndogs,
                                     data = ds, else_condition = "other")
         expect_equal(as.vector(ds$new1), factor(c("Jasmine", "other", "2", "3", "Zeus", "2", "2", "3",
                                    "2", "2", "2", "other", "3", "Belgium", "6", "Fluffy",
@@ -134,7 +142,7 @@ with_test_authentication({
     test_that("conditionalTransform with text", {
         ds$new2 <- conditionalTransform(ndogs < 1 ~ country,
                                     ndogs == 1 ~ q3,
-                                    ndogs > 1 ~ ndogs, 
+                                    ndogs > 1 ~ ndogs,
                                     data = ds, type = "text")
         expect_equal(as.vector(ds$new2), c("Jasmine", NA, "2", "3", "Zeus", "2", "2", "3",
                                    "2", "2", "2", NA, "3", "Belgium", "6", "Fluffy",
