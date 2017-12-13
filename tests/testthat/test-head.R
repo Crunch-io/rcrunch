cea88d8a4b33e9e6236b020a0b0042eb05461d6a
GShotwell <gordon@crunch.io>
2017-12-13 14:03:51 -0400

    Added Unit tests

--- tests/testthat/test-head.R
+++ tests/testthat/test-head.R
@@ -1,5 +1,25 @@
 context("head and tail")
 
+with_mock_crunch({
+    ds <- loadDataset("test ds")
+    ds2 <- ds[, "textVar"]
+    test_that("head method works on CrunchDatasets", {
+        head_df <- head(ds2)
+        expect_is(head_df, "data.frame")
+        expect_equal(nrow(head_df), 6)
+        expect_identical(head_df$textVar, c("w", "n", "x", "b", "q", "s"))
+    })
+    test_that("head method works on CrunchDataFrame", {
+        expect_identical(head(ds2), head(as.data.frame(ds2)))
+    })
+    test_that("tail generates the correct request", {
+        expect_GET(tail(as.data.frame(ds)),
+            'https://app.crunch.io/api/datasets/1/variables/birthyr/values',
+            '?filter=%7B%22function%22%3A%22between%22%2C%22args%22%3A%5B%7B%22function%22%3A%22row%22%2C%22args%22%3A%5B%5D%7D%2C%7B%22value%22%3A19%7D%2C%7B%22value%22%3A25%7D%5D%7D&offset=0&limit=100000')
+    })
+    expect_identical(tail(ds$textVar), c("i", "h", "z", "m", "c", "x"))
+})
+
 with_test_authentication({
     ds <- newDatasetFromFixture("apidocs")
     df <- as.data.frame(ds, force = TRUE)
