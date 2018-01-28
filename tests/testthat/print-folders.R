572060c403d1b8546f9591b020e257335e930eed
Neal Richardson <neal@crunch.io>
2018-01-28 14:27:49 -0800

    Decode u-codes in tests now that they're hidden from CRAN

--- tests/testthat/print-folders.R
+++ tests/testthat/print-folders.R
@@ -1,54 +1,54 @@
-## These are sadly unreadable thanks to R's archaic restriction on UTF-8
+## These are moved from test-variable-folder.R thanks to R's archaic restriction on UTF-8
 testthat::expect_output(print(folders(ds), depth=2),
 "/
-\u251C\u2500\u2500 Group 1/
-\u2502   \u251C\u2500\u2500 Birth Year
-\u2502   \u251C\u2500\u2500 Nested/
-\u2502   \u2502   \u251C\u2500\u2500 Gender
-\u2502   \u2502   \u251C\u2500\u2500 Categorical Location
-\u2502   \u2502   \u2514\u2500\u2500 mymrset
-\u2502   \u2514\u2500\u2500 Text variable ftw
-\u2514\u2500\u2500 Group 2/
-    \u251C\u2500\u2500 starttime
-    \u2514\u2500\u2500 Cat Array", fixed=TRUE
+├── Group 1/
+│   ├── Birth Year
+│   ├── Nested/
+│   │   ├── Gender
+│   │   ├── Categorical Location
+│   │   └── mymrset
+│   └── Text variable ftw
+└── Group 2/
+    ├── starttime
+    └── Cat Array", fixed=TRUE
 )
 
 testthat::expect_output(print(folders(ds), depth=1),
 "/
-\u251C\u2500\u2500 Group 1/
-\u2502   \u251C\u2500\u2500 Birth Year
-\u2502   \u251C\u2500\u2500 Nested/
-\u2502   \u2514\u2500\u2500 Text variable ftw
-\u2514\u2500\u2500 Group 2/
-    \u251C\u2500\u2500 starttime
-    \u2514\u2500\u2500 Cat Array", fixed=TRUE
+├── Group 1/
+│   ├── Birth Year
+│   ├── Nested/
+│   └── Text variable ftw
+└── Group 2/
+    ├── starttime
+    └── Cat Array", fixed=TRUE
 )
 
 testthat::expect_output(print(folders(ds), pretty=TRUE),
 "/
-\u251C\u2500\u2500 Group 1/
-\u2514\u2500\u2500 Group 2/", fixed=TRUE
+├── Group 1/
+└── Group 2/", fixed=TRUE
 )
 
 testthat::expect_output(print(folders(ds)[["Group 1/Nested"]], pretty=TRUE),
 "/Group 1/Nested/
-\u251C\u2500\u2500 Gender
-\u251C\u2500\u2500 Categorical Location
-\u2514\u2500\u2500 mymrset", fixed=TRUE
+├── Gender
+├── Categorical Location
+└── mymrset", fixed=TRUE
 )
 
 with(temp.option(crunch.delimiter="|"), {
     testthat::expect_output(print(folders(ds), depth=2),
 "|
-\u251C\u2500\u2500 Group 1|
-\u2502   \u251C\u2500\u2500 Birth Year
-\u2502   \u251C\u2500\u2500 Nested|
-\u2502   \u2502   \u251C\u2500\u2500 Gender
-\u2502   \u2502   \u251C\u2500\u2500 Categorical Location
-\u2502   \u2502   \u2514\u2500\u2500 mymrset
-\u2502   \u2514\u2500\u2500 Text variable ftw
-\u2514\u2500\u2500 Group 2|
-    \u251C\u2500\u2500 starttime
-    \u2514\u2500\u2500 Cat Array", fixed=TRUE
+├── Group 1|
+│   ├── Birth Year
+│   ├── Nested|
+│   │   ├── Gender
+│   │   ├── Categorical Location
+│   │   └── mymrset
+│   └── Text variable ftw
+└── Group 2|
+    ├── starttime
+    └── Cat Array", fixed=TRUE
     )
 })
