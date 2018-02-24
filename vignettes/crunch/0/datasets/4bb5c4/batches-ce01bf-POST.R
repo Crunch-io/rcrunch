033fdb81a1f6e54ce0ee0fa6f10562de59ccbcef
Neal Richardson <neal@crunch.io>
2018-02-24 14:46:25 -0800

    Vignette progress

---
+++ vignettes/crunch/0/datasets/4bb5c4/batches-ce01bf-POST.R
@@ -0,0 +1,29 @@
+structure(list(url = "/datasets/4bb5c4/batches/", status_code = 202L, 
+    headers = structure(list(allow = "GET, HEAD, OPTIONS, POST", 
+        `content-encoding` = "gzip", `content-type` = "application/json;charset=utf-8", 
+        date = "Sat, 24 Feb 2018 22:15:04 GMT", location = "/datasets/4bb5c4/batches/1/", 
+        server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
+        `x-timing` = "", `content-length` = "180", connection = "keep-alive"), .Names = c("allow", 
+    "content-encoding", "content-type", "date", "location", "server", 
+    "set-cookie", "vary", "x-timing", "content-length", "connection"
+    ), class = c("insensitive", "list")), all_headers = list(
+        structure(list(status = 202L, version = "HTTP/1.1", headers = structure(list(
+            allow = "GET, HEAD, OPTIONS, POST", `content-encoding` = "gzip", 
+            `content-type` = "application/json;charset=utf-8", 
+            date = "Sat, 24 Feb 2018 22:15:04 GMT", location = "/datasets/4bb5c4/batches/1/", 
+            server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
+            `x-timing` = "", `content-length` = "180", connection = "keep-alive"), .Names = c("allow", 
+        "content-encoding", "content-type", "date", "location", 
+        "server", "set-cookie", "vary", "x-timing", "content-length", 
+        "connection"), class = c("insensitive", "list"))), .Names = c("status", 
+        "version", "headers"))), cookies = structure(list(domain = ".crunch.io", 
+        flag = TRUE, path = "/", secure = FALSE, expiration = structure(1551046504, class = c("POSIXct", 
+        "POSIXt")), name = "token", value = "REDACTED"), .Names = c("domain", 
+    "flag", "path", "secure", "expiration", "name", "value"), row.names = c(NA, 
+    -1L), class = "data.frame"), content = charToRaw("{\"element\": \"shoji:view\", \"self\": \"/datasets/4bb5c4/batches/\", \"value\": \"/progress/\"}"), 
+    date = structure(1519510504, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = structure(c(0, 4.3e-05, 4.5e-05, 
+    0.00011, 0.393072, 0.393154), .Names = c("redirect", "namelookup", 
+    "connect", "pretransfer", "starttransfer", "total"))), .Names = c("url", 
+"status_code", "headers", "all_headers", "cookies", "content", 
+"date", "times"), class = "response")
