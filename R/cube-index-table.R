e74b6fc5bbeab5d61d78ee937c1224144a219102
Jonathan Keane <jon@crunch.io>
2018-03-21 14:48:37 -0500

    Clean up of index table

--- R/cube-index-table.R
+++ R/cube-index-table.R
@@ -3,12 +3,34 @@
 #' Index tables are percentages of percentages. They take the percentage from
 #' `prop.table()` and divide that by the proportions of the other margin.
 #'
-#' `index.table()` is only implemented for 2 dimensional cubes.
+#' `index.table()` is only implemented for 2 dimensional cubes. If you need to
+#' calculate indexes for a higher dimension Cube, please slice the cube first.
 #'
 #' @param x A CrunchCube to calculate index table for
 #' @param margin which margin to index against (1 for rows, 2 for columns)
 #'
 #' @return an array of percentages indexed to the margin provided
+#'
+#' @examples
+#' \dontrun{
+#'  cube_object
+#'  #    v7
+#'  # v4  C E
+#'  #   B 5 2
+#'  #   C 5 3
+#'  index.table(cube_object, 1)
+#'  #    v7
+#'  # v4         C         E
+#'  #   B 1.071429 0.8571429
+#'  #   C 0.937500 1.1250000
+#'  index.table(cube_object, 2)
+#'  #    v7
+#'  # v4         C         E
+#'  #   B 1.071429 0.8571429
+#'  #   C 0.937500 1.1250000
+#'  #
+#' }
+#'
 #' @export
 index.table <- function (x, margin) {
     if (length(dim(x)) != 2){
@@ -20,7 +42,7 @@ index.table <- function (x, margin) {
     marg <- sweep(margin.table(x, other_margin), 1, margin.table(x), "/")
 
     if (identical(dim(tab), dim(marg))) {
-        # if the dimensions are exactly the same, use diret division
+        # if the dimensions are the same, use direct division, needed for MRs
         return(tab/marg)
     } else {
         return(sweep(tab, other_margin, marg, "/"))
