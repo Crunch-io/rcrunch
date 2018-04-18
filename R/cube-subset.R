587b8a02b0657e2477514405dba0be2ecbd48d08
GShotwell <gordon@crunch.io>
2018-04-18 17:00:28 -0400

    Additional comments

--- R/cube-subset.R
+++ R/cube-subset.R
@@ -1,13 +1,18 @@
 #' @rdname cube-methods
 #' @export
 setMethod("[", "CrunchCube", function (x, i, j, ..., drop = TRUE) {
+    # Missing arguments to a subset method means "select all the items along this
+    # dimension". In order to do this we need to capture the unevaluated arguments
+    # and replace all the missing elements of that list with TRUE.
     if (nargs() == 2) {
         index <- eval(substitute(alist(i)))
     } else {
         index <- eval(substitute(alist(i, j, ...)))
     }
     index <- replaceMissingWithTRUE(index)
+
     dims <- dim(x)
+    #Check if the user has supplied the right number of dimensions
     if (length(index) != length(dims)) {
         halt("You must supply ",
              length(dims),
@@ -18,6 +23,9 @@ setMethod("[", "CrunchCube", function (x, i, j, ..., drop = TRUE) {
              ".")
     }
 
+    # This block of code checks whether the user has supplied a valid index. For
+    # instance it will error if they tried to select element 4 from a dimension
+    # with only three elements.
     err_indices <- lapply(index, function(idx) {
         if (is.numeric(idx)) {
             return(max(idx))
@@ -49,24 +57,31 @@ setMethod("[", "CrunchCube", function (x, i, j, ..., drop = TRUE) {
              paste0(errs[1:min(sum(invalid_indices), 5)], collapse = "\n"))
     }
 
+    # We then translate the index which the user supplied of the user cube
+    # to the dimensionality of the real cube. See documentation to translateCubeIndex
     translated_index <- translateCubeIndex(x, index, drop)
 
+    # The index needs to be further transformed to skip over missing categories.
+    # See the "Subsetting Crunch Cubes" vignette for more on this.
     if (x@useNA != "always") {
         translated_index <- skipMissingCategories(x, translated_index)
     }
-
+    # Finally we can use that translated subset on the real cube.
     out <- x
     out@arrays[] <- lapply(out@arrays, function(arr){
-        # selecte_dims must be FALSE here because we do not want to treat them
+        # select_dims must be FALSE here because we do not want to treat them
         # differently
         subsetCubeArray(arr, translated_index, drop, selected_dims = FALSE)
     })
 
+    # We also need to subset the @dims part of the object using the same index.
+    # This is so that other cube methods, like as.array, don't get confused.
     out@dims[] <- mapply(subsetArrayDimension,
                          dim = x@dims,
                          idx = translated_index,
                          SIMPLIFY = FALSE)
-
+    # Some dimensions need to be dropped because they were dropped from the
+    # cube@array slot.
     if (drop) {
         keep_args <- vapply(translated_index, function(a) {
             if (is.logical(a)) {
@@ -82,7 +97,7 @@ setMethod("[", "CrunchCube", function (x, i, j, ..., drop = TRUE) {
 
 #' Replace missing elements with TRUE
 #'
-#' When subsetting missingness stands in for selecting all of the elements of that
+#' When subsetting, missingness stands in for selecting all of the elements of that
 #' dimension. This gets tricky when you are selecting n dimensions and so need to
 #' capture the dimensions with `...`.  This function checks if an element of a list
 #' is missing and replaces that element with `TRUE` in order to handle this case.
@@ -133,16 +148,22 @@ replaceMissingWithTRUE <- function(l){
 #' @return a list of array extent indices (for the real-cube)
 translateCubeIndex <- function(x, subset, drop) {
     is_selected <- is.selectedDimension(x@dims)
-    prog_names <- names(is_selected) #the real cube
-    # the user facing cube
+    #the real cube dimensions
+    prog_names <- names(is_selected)
+    # the user cube dimensions
     user_names <- prog_names[!is_selected]
     if (length(prog_names) == length(user_names)) {
         #no MR variables so no need to translate the subset
         return(subset)
     }
+    # This is the main work of the subset translation, just taking the user
+    # supplied subset and projecting them up to real cube dimension
     out <- as.list(rep(TRUE, length(prog_names)))
     out[match(user_names, prog_names)] <- subset
 
+    # Dropping MR variables is a bit special. Whenever the user drops the MR dimension
+    # the MR selection dimension is also dropped. This checks if it's an MR dimension
+    # which is being dropped, and assigns an appropriate index value.
     if (drop) {
         for (i in seq_along(prog_names)) {
             if (i == 1) {
@@ -171,7 +192,8 @@ translateCubeIndex <- function(x, subset, drop) {
 #' user subsets a cube with a missing category, we need to translate that subset
 #' to only refer to the non-missing categories. This only occurs if
 #' `cube@useMissing` is set to `"no"`. This function handles this behavior by
-#' translating the user supplied indices to logical vectors.
+#' translating the user supplied indices to logical vectors which accounts for the
+#' missing values.
 #'
 #' @param cube a CrunchCube
 #' @param index a index of the real cube which was generated by
