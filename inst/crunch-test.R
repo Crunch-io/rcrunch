fca8c944e04f868d4da2c6b731b8811e5613636a
Neal Richardson <neal@crunch.io>
2018-04-26 11:35:24 -0700

    Remove session_store and some unnecessary checks (#241)

--- inst/crunch-test.R
+++ inst/crunch-test.R
@@ -31,14 +31,12 @@ cubify <- function (..., dims) {
 
 ## Contexts
 with_mock_crunch <- function (expr) {
-    env <- parent.frame()
-    with(temp.options(crunch.api="https://app.crunch.io/api/",
-                      httptest.mock.paths=c(".", "../inst/", system.file(package="crunch"))), {
-        with_mock_api({
-            try(crunch:::warmSessionCache())
-            eval(expr, envir=env)
-        })
-    })
+    opts <- temp.options(
+        crunch.api="https://app.crunch.io/api/",
+        httptest.mock.paths=c(".", "../inst/", system.file(package="crunch"))
+    )
+    with(opts,
+        with_mock_API(expr))
 }
 
 with_POST <- function (resp, expr) {
