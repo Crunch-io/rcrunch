60935dbd9a903a92b0a06ec3eef450be19820881
GShotwell <gordon@crunch.io>
2018-04-30 15:01:30 -0300

    Vignette mocks updated, not working
    Getting a missing error at line 180 when knitting even though it looks like the mock is recorded.

---
+++ vignettes/crunch/0/api/sources-POST.R
@@ -0,0 +1,28 @@
+structure(list(url = "/api/sources/", status_code = 201L, headers = structure(list(
+    allow = "GET, HEAD, OPTIONS, POST", `content-encoding` = "gzip", 
+    `content-type` = "application/json;charset=utf-8", date = "Mon, 30 Apr 2018 17:50:41 GMT", 
+    location = "/api/sources/6232e8/", server = "nginx", `set-cookie` = "REDACTED", 
+    vary = "Cookie, Accept-Encoding", `x-timing` = "", `content-length` = "86", 
+    connection = "keep-alive"), .Names = c("allow", "content-encoding", 
+"content-type", "date", "location", "server", "set-cookie", "vary", 
+"x-timing", "content-length", "connection"), class = c("insensitive", 
+"list")), all_headers = list(structure(list(status = 201L, version = "HTTP/1.1", 
+    headers = structure(list(allow = "GET, HEAD, OPTIONS, POST", 
+        `content-encoding` = "gzip", `content-type` = "application/json;charset=utf-8", 
+        date = "Mon, 30 Apr 2018 17:50:41 GMT", location = "/api/sources/6232e8/", 
+        server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
+        `x-timing` = "", `content-length` = "86", connection = "keep-alive"), .Names = c("allow", 
+    "content-encoding", "content-type", "date", "location", "server", 
+    "set-cookie", "vary", "x-timing", "content-length", "connection"
+    ), class = c("insensitive", "list"))), .Names = c("status", 
+"version", "headers"))), cookies = structure(list(domain = ".crunch.io", 
+    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1556646641, class = c("POSIXct", 
+    "POSIXt")), name = "token", value = "REDACTED"), .Names = c("domain", 
+"flag", "path", "secure", "expiration", "name", "value"), row.names = c(NA, 
+-1L), class = "data.frame"), content = charToRaw("{\"element\": \"shoji:view\", \"self\": \"/api/sources/\"}"), 
+    date = structure(1525110641, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = structure(c(0, 4.8e-05, 5e-05, 
+    0.000258, 0.00027, 0.964295), .Names = c("redirect", "namelookup", 
+    "connect", "pretransfer", "starttransfer", "total"))), .Names = c("url", 
+"status_code", "headers", "all_headers", "cookies", "content", 
+"date", "times"), class = "response")
