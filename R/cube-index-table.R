cfad56c1b60115411bfb3ffa4b807084e73acde4
Jonathan Keane <jkeane@gmail.com>
2018-06-18 13:39:52 -0500

    Update to index.table to use the correct formula, including collapse.dimensions

--- R/cube-index-table.R
+++ R/cube-index-table.R
@@ -1,7 +1,7 @@
 #' Calculate an index table for a CrunchCube
 #'
 #' Index tables are percentages of percentages. They take the percentage from
-#' `prop.table()` and divide that by the proportions of the other margin.
+#' `prop.table(cube, margin)` and, by default, divide that by the proportions of the other margin. The `baseline` argument can be used to provide baseline proportions to compare against.
 #'
 #' `index.table()` is only implemented for 2 dimensional cubes. If you need to
 #' calculate indexes for a higher dimension Cube, please slice the cube first.
@@ -29,6 +29,11 @@
 #'  # v4    C   E
 #'  #   B 100  80
 #'  #   C 100 120
+#'  index.table(cube_object, 2, c(0.6, 0.4))
+#'  #    v7
+#'  # v4          C         E
+#'  #   B  83.33333  66.66667
+#'  #   C 125.00000 150.00000
 #' }
 #'
 #' @export
@@ -36,13 +41,24 @@ index.table <- function (x, margin, baseline) {
     if (length(dim(x)) != 2){
         halt("Index tables can only be calculated for 2 dimensional cubes.")
     }
+    other_margin <- 3 - margin ## Assumes 2-D
 
+    # the numerators are the proportions by the margin axis
+    tab <- prop.table(x, margin)
+    
+    # the denominators are the proportions of the uni-variate cube based on the
+    # other margin. Because collapse.dimensions collapses the dimensions given
+    # in margin, we get a uni(variate )cube for the opposite margin. Only do
+    # this if an explicit baseline wasn't specified.
     if (missing(baseline)) {
         unicube <- collapse.dimensions(x, margin)
         baseline <- prop.table(unicube)
     }
 
-    tab <- prop.table(x, margin)
-
-    return(tab/broadcast(baseline, dim(tab)) * 100)
+    if (identical(dim(tab), dim(baseline))) {
+        # if the dimensions are the same, use direct division, needed for MRs
+        return(tab/baseline * 100)
+    } else {
+        return(sweep(tab, other_margin, baseline, "/") * 100)
+    }
 }
