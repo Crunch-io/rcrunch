5290181f98257750121f386dc09babd2a14516be
GShotwell <gordon@crunch.io>
2018-07-10 12:38:26 -0300

    title, subtitle, and name methods

--- R/decks.R
+++ R/decks.R
@@ -9,18 +9,74 @@ setMethod("[[", "DeckCatalog",  function (x, i, ...) {
     getEntity(x, i, CrunchDeck, ...)
 })
 
-setMethod("slides", "CrunchDeck", function(x) {
+setMethod("slides", "CrunchDeck", function (x) {
     SlideCatalog(crGET(shojiURL(x, "catalogs", "slides")))
 })
 
-setMethod("export", "CrunchDeck", function(x, path = "test") {
-browser()
-    dl_link <- crPOST(x@urls$export_url, config = add_headers(`Accept`="application/json"))
-    download.file(dl_link, paste0(path, name(x), ".json"))
-    return(out)
+setMethod("name", "CrunchDeck", function(x) x@body$name)
+setMethod("name<-", "CrunchDeck", function(x, value) {
+    stopifnot(is.character(value))
+    stopifnot(length(value) == 1)
+    x@body$name <- value
+    crPATCH(self(x), body = toJSON(x@body))
+    invisible(refresh(x))
+    })
+
+setMethod("titles", "CrunchDeck", function (x){
+    titles(slides(x))
 })
 
-setMethod("[[", "SlideCatalog", function(x, i, ...) {
+setMethod("titles<-", "CrunchDeck", function (x, value){
+    slides <- slides(x)
+    titles(slides) <- value
+    invisible(refresh(x))
+})
+
+setMethod("subtitles", "CrunchDeck", function (x){
+     subtitles(slides(x))
+})
+
+setMethod("subtitles<-", "CrunchDeck", function (x, value){
+    slides <- slides(x)
+    subtitles(slides) <- value
+    invisible(refresh(x))
+})
+setMethod("titles", "SlideCatalog", function (x){
+    as.character(lapply(x@index, function(x) x$title))
+})
+
+setMethod("titles<-", "SlideCatalog", function (x, value){
+    updateSlideTitle(x, value, "title")
+})
+
+setMethod("subtitles", "SlideCatalog", function (x){
+    as.character(lapply(x@index, function(x) x$subtitle))
+})
+
+setMethod("subtitles<-", "SlideCatalog", function (x, value){
+    updateSlideTitle(x, value, "subtitle")
+})
+
+updateSlideTitle <- function(x, value, type) {
+    stopifnot(is.character(value))
+    payload <- mapply(function (item, new_title){
+        item[[type]] <- new_title
+        return(item)
+    }, x@index, value, SIMPLIFY = FALSE)
+    crPATCH(self(x), body = toJSON(payload))
+    invisible(refresh(x))
+}
+
+setMethod("export", "CrunchDeck", function (x, path) {
+    # This is to avoid having a default argument in the generic
+    if (missing(path)) {
+        path <- ""
+    }
+    dl_link <- crPOST(x@urls$export_url, config = add_headers(`Accept`="application/json"))
+    download.file(dl_link, paste0(path, name(x), ".json"))
+})
+
+setMethod("[[", "SlideCatalog", function (x, i, ...) {
   getEntity(x, i, CrunchSlide)
 })
 
