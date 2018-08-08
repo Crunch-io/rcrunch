747bec138648c4da58456d40c9b99246d4da507c
Neal Richardson <neal@crunch.io>
2018-08-08 14:10:34 -0700

    Remove boolean support (long live 3vl)

--- R/variable-to-R.R
+++ R/variable-to-R.R
@@ -63,11 +63,6 @@ parse_column <- list(
     datetime = function(col, variable, mode) {
         out <- columnParser("text")(col)
         return(from8601(out))
-    },
-    boolean = function(col, variable, mode) {
-        missings <- vapply(col, Negate(is.logical), logical(1))
-        col[missings] <- NA
-        return(as.logical(unlist(col)))
     }
 )
 columnParser <- function(vartype) {
