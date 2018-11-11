b7ca3b3c696357c51e5bfb9e107cc5df7f39bf0a
Jonathan Keane <jon@crunch.io>
2018-11-12 08:39:08 +0900

    function creation

--- R/teams.R
+++ R/teams.R
@@ -111,52 +111,50 @@ setMethod("delete", "CrunchTeam", function(x, ...) {
 #' @export
 setGeneric("team", function(x) standardGeneric("team"))
 
+.getTeam <- function(x) {
+    if (is.null(x@body$team)) {
+        return(NULL)
+    }
+    
+    return(CrunchTeam(crGET(x@body$team)))
+}
+
+.setTeam <- function(x, value) { return(setEntitySlot(x, "team", value)) }
+
 #' @rdname team-sharing
 #' @export
 setGeneric("team<-", function(x, value) standardGeneric("team<-"))
 
 #' @rdname team-sharing
 #' @export
-setMethod("team", "CrunchFilter", function(x) {
-    if (is.null(x@body$team)) {
-        return(NULL)
-    }
-    
-    return(CrunchTeam(crGET(x@body$team)))
-})
+setMethod("team", "CrunchFilter", .getTeam)
 
 #' @rdname team-sharing
 #' @export
 setMethod("team<-", c("CrunchFilter", "CrunchTeam"), function(x, value) {
-    return(setEntitySlot(x, "team", self(value)))
+    return(.setTeam(x, self(value)))
 })
 
 #' @rdname team-sharing
 #' @export
 setMethod("team<-", c("CrunchFilter", "ANY"), function(x, value) {
     # TODO: check if value is at least something like a URL?
-    return(setEntitySlot(x, "team", value))
+    return(.setTeam(x, value))
 })
 
 #' @rdname team-sharing
 #' @export
-setMethod("team", "Multitable", function(x) {
-    if (is.null(x@body$team)) {
-        return(NULL)
-    }
-    
-    return(CrunchTeam(crGET(x@body$team)))
-})
+setMethod("team", "Multitable", .getTeam)
 
 #' @rdname team-sharing
 #' @export
 setMethod("team<-", c("Multitable", "CrunchTeam"), function(x, value) {
-    return(setEntitySlot(x, "team", self(value)))
+    return(.setTeam(x, self(value)))
 })
 
 #' @rdname team-sharing
 #' @export
 setMethod("team<-", c("Multitable", "ANY"), function(x, value) {
     # TODO: check if value is at least something like a URL?
-    return(setEntitySlot(x, "team", value))
+    return(.setTeam(x, value))
 })
