eb8546bc9921bb978cc2011cdef5d75384daecc0
Neal Richardson <neal@crunch.io>
2019-03-01 16:19:52 -0800

    Tweak cleanImportedArray after manual testing

--- R/clean-array.R
+++ R/clean-array.R
@@ -1,5 +1,5 @@
-#' Clean up an array variable imported from a lossy file format 
-#' 
+#' Clean up an array variable imported from a lossy file format
+#'
 #' Array and multiple-response variables coming in from SPSS or other file
 #' formats generally need some work to reconstruct the "right" metadata because
 #' they have to shove both parent and subvariable metadata into the "varlabels"
@@ -7,29 +7,31 @@
 #' prefix containing the parent question wording (description) and a suffix that
 #' is the actual response label.
 #'
-#' This function detects this prefix and reconstructs what may have been the 
+#' This function detects this prefix and reconstructs what may have been the
 #' original array definition.
 #'
 #' @param variable An array Variable
-#' @return `variable` with edits pushed to the API. A common prefix on 
+#' @param min.prefix.length Integer: how many characters long does the common
+#' string need to be in order to consider it significant enough to use? Default
+#' is 20.
+#' @return `variable` with edits pushed to the API. A common prefix on
 #' subvariable names is extracted and set as the variable's description.
 #' @export
-cleanImportedArray <- function (variable) {
+cleanImportedArray <- function (variable, min.prefix.length=20) {
     if (length(subvariables(variable)) > 1) {
-        MIN_PREFIX_LENGTH <- 20 # TODO: tune/make configurable
         prefix <- findCommonPrefix(names(subvariables(variable)))
         # If length of the common stem is enough, extract it,
         # remove it from the subvar names,
         # remove trailing whitespace/punctuation,
         # and set it as variable description.
-        if (nchar(prefix) >= MIN_PREFIX_LENGTH) {
+        if (nchar(prefix) >= min.prefix.length) {
             # Use wildcard regexp with length just in case there are special chars in prefix.
             # We already know that the prefix matches.
             re <- paste0("^.{", nchar(prefix), "}")
             names(subvariables(variable)) <- sub(re, "", names(subvariables(variable)))
             # Now, remove whitespace and some punctuation from end of prefix, but
             # don't remove a question mark or other reasonable punctuation
-            prefix <- sub("[[:space:]\\-\\:;]*$", "", prefix)
+            prefix <- sub("[[:space:]\\-\\:;|]*$", "", prefix)
             description(variable) <- prefix
         }
     }
