a803c96c8fc1c881d1524f7db5f5b7c86c79fe1d
Jonathan Keane <jkeane@gmail.com>
2019-03-15 10:42:44 -0500

    Added travis.install script

---
+++ .travis.install.sh
@@ -0,0 +1,7 @@
+choco install -y r.project
+export R_VERSION=`ls 'C:\Program Files\R\'`
+export PATH=$PATH:';C:\Program Files\R\'$R_VERSION'\bin\x64'
+echo 'options(repos = "https://cloud.r-project.org")' > ~/.Rprofile.site
+export R_PROFILE=~/.Rprofile.site
+doskey R=R.exe
+doskey Rscript=Rscript.exe
