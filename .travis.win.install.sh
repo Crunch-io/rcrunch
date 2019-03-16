2d928705cd2e1aa924a7f88e9e0c54a474d79cbf
Jonathan Keane <jkeane@gmail.com>
2019-03-16 15:33:22 -0500

    moar .exe

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -5,8 +5,6 @@ export R_VERSION=`ls 'C:\Program Files\R\'` &&
 export PATH=$PATH:';C:\Program Files\R\'$R_VERSION'\bin\x64' &&
 echo 'options(repos = "https://cloud.r-project.org")' > ~/.Rprofile.site &&
 export R_PROFILE=~/.Rprofile.site &&
-doskey R=R.exe &&
-doskey Rscript=Rscript.exe &&
 
 Rscript -e 'sessionInfo()' &&
 Rscript -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
