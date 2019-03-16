0b7427ff951d4361a3e091512966564e280e9f47
Jonathan Keane <jkeane@gmail.com>
2019-03-15 22:09:34 -0500

    makefiles quote/escapeing for windows is not happy

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
 choco install -y r.project &&
-choco install -y make && 
+choco install -y make &&
 export R_VERSION=`ls 'C:\Program Files\R\'` &&
 export PATH=$PATH:';C:\Program Files\R\'$R_VERSION'\bin\x64' &&
 echo 'options(repos = "https://cloud.r-project.org")' > ~/.Rprofile.site &&
 export R_PROFILE=~/.Rprofile.site &&
 doskey R=R.exe &&
 doskey Rscript=Rscript.exe
+
+Rscript -e 'sessionInfo()'
+Rscript -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}'
+Rscript -e 'devtools::session_info(installed.packages()[, "Package"])'
