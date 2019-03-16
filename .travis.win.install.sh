31ffe5197f24fe480063140cfece852a4d278e4d
Jonathan Keane <jkeane@gmail.com>
2019-03-16 15:53:13 -0500

    even more .exe

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -6,6 +6,6 @@ export PATH=$PATH:';C:\Program Files\R\'$R_VERSION'\bin\x64' &&
 echo 'options(repos = "https://cloud.r-project.org")' > ~/.Rprofile.site &&
 export R_PROFILE=~/.Rprofile.site &&
 
-Rscript -e 'sessionInfo()' &&
-Rscript -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
-Rscript -e 'devtools::session_info(installed.packages()[, "Package"])'
+Rscript.exe -e 'sessionInfo()' &&
+Rscript.exe -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
+Rscript.exe -e 'devtools::session_info(installed.packages()[, "Package"])'
