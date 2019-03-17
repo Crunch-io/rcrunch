efd21ec3531aea0317766dd4836e4e009c5f0d06
Jonathan Keane <jkeane@gmail.com>
2019-03-17 15:47:48 -0500

    Move env vars to global vars

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -9,8 +9,6 @@ export R_PROFILE=~/.Rprofile.site &&
 Rscript.exe -e 'sessionInfo()' &&
 Rscript.exe -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
 Rscript.exe -e 'deps <- devtools::dev_package_deps(dependencies = NA);devtools::install_deps(dependencies = TRUE)' &&
-Rscript.exe -e 'devtools::session_info(installed.packages()[, "Package"])' && 
+Rscript.exe -e 'devtools::session_info(installed.packages()[, "Package"])' &&
 
-export PKG_TARBALL="$(perl -ne '$version = $1 if (/^Version:\s(\S+)/); $package = $1 if (/^Package:\s*(\S+)/); END { print "${package}_$version.tar.gz" }' DESCRIPTION)" &&
-export _R_CHECK_CRAN_INCOMING_=FALSE &&
-export R_QPDF=true
+export PKG_TARBALL="$(perl -ne '$version = $1 if (/^Version:\s(\S+)/); $package = $1 if (/^Package:\s*(\S+)/); END { print "${package}_$version.tar.gz" }' DESCRIPTION)" 
