b8a6a3c1388099ce6cadbbaa2e8288386f64d5c8
Jonathan Keane <jkeane@gmail.com>
2019-03-20 10:13:50 -0500

    type both

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -8,7 +8,7 @@ export R_PROFILE=~/.Rprofile.site &&
 
 Rscript.exe -e 'sessionInfo()' &&
 Rscript.exe -e 'install.packages("devtools");if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
-Rscript.exe -e 'devtools::install_deps(dependencies = TRUE, type = "source")' &&
+Rscript.exe -e 'devtools::install_deps(dependencies = TRUE, type = "both")' &&
 Rscript.exe -e 'devtools::session_info(installed.packages()[, "Package"])' &&
 
 export PKG_TARBALL="$(perl -ne '$version = $1 if (/^Version:\s(\S+)/); $package = $1 if (/^Package:\s*(\S+)/); END { print "${package}_$version.tar.gz" }' DESCRIPTION)"
