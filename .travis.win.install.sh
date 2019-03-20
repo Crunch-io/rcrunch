f8cf4c3fdeae0907a068deae5e49532c06ebe664
Jonathan Keane <jkeane@gmail.com>
2019-03-20 16:11:43 -0500

    cleanup

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -10,12 +10,8 @@ export R_PROFILE=~/.Rprofile.site &&
 echo which make &&
 
 Rscript.exe -e 'sessionInfo()' &&
-Rscript.exe -e '!nzchar(Sys.which(Sys.getenv("MAKE", "make")))' &&
 Rscript.exe -e 'install.packages("devtools", dependencies = TRUE);if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
-echo 'one' &&
 Rscript.exe -e 'deps <- devtools::dev_package_deps(dependencies = TRUE); inst <- installed.packages(); install.packages(deps$package[!deps$package %in% inst[,"Package"]])' &&
-echo 'two' &&
 Rscript.exe -e 'devtools::session_info(installed.packages()[, "Package"])' &&
-echo 'three' &&
 
 export PKG_TARBALL="$(perl -ne '$version = $1 if (/^Version:\s(\S+)/); $package = $1 if (/^Package:\s*(\S+)/); END { print "${package}_$version.tar.gz" }' DESCRIPTION)"
