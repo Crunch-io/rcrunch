cc892172509fb5d0caf37a3953c3088f7cc4e088
Jonathan Keane <jkeane@gmail.com>
2019-03-20 15:41:30 -0500

    never build from source

--- .travis.win.install.sh
+++ .travis.win.install.sh
@@ -4,7 +4,7 @@ choco install -y r.project &&
 choco install -y make &&
 export R_VERSION=`ls 'C:\Program Files\R\'` &&
 export PATH=$PATH:';C:\Program Files\R\'$R_VERSION'\bin\x64' &&
-echo 'options(repos = "https://cloud.r-project.org", install.packages.compile.from.source = "always")' > ~/.Rprofile.site &&
+echo 'options(repos = "https://cloud.r-project.org", install.packages.compile.from.source = "never")' > ~/.Rprofile.site &&
 export R_PROFILE=~/.Rprofile.site &&
 
 echo which make &&
