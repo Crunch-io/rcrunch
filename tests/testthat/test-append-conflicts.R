0086f886baaf00870a1c5623237a0cd678f7ef02
Alessandro Molina <amol@turbogears.org>
2019-05-29 16:31:26 +0200

    Adapt tests, batch 0 exists no more

--- tests/testthat/test-append-conflicts.R
+++ tests/testthat/test-append-conflicts.R
@@ -7,7 +7,7 @@ with_test_authentication({
         alias(part2$mr_3) <- "CA"
         name(part2$CA) <- "Bad var"
         test_that("setup for append array type mismatch", {
-            expect_length(batches(part1), 2)
+            expect_length(batches(part1), 1)
             expect_true("CA" %in% names(part1))
             expect_true("CA" %in% names(part2))
             expect_true(is.CA(part1$CA))
@@ -15,8 +15,7 @@ with_test_authentication({
             expect_prints(
                 batches(part1),
                 get_output(data.frame(
-                    id = c(0, 1),
-                    status = c("imported", "imported")
+                    status = c("imported")
                 ))
             )
         })
@@ -28,8 +27,7 @@ with_test_authentication({
             expect_prints(
                 batches(part1),
                 get_output(data.frame(
-                    id = c(0, 1),
-                    status = c("imported", "imported")
+                    status = c("imported")
                 ))
             )
 
@@ -45,8 +43,7 @@ with_test_authentication({
             expect_prints(
                 batches(part1),
                 get_output(data.frame(
-                    id = c(0, 1),
-                    status = c("imported", "imported")
+                    status = c("imported", "error")
                 ))
             )
         })
