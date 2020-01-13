f1207186713e74d2da714853e50e3db61783f0f5
Greg Freedman Ellis <greg@crunch.io>
2020-01-13 12:40:48 -0600

    oops nolint got passed through here

--- R/R-to-variable.R
+++ R/R-to-variable.R
@@ -17,8 +17,8 @@ NULL
 #' you can declare methods for `toVariable`.
 #' @param x An R vector you want to turn into a Crunch variable
 #' @param ... Additional metadata fields for the variable, such as "name" and
-#' "description". See the
-#' [API documentation](http://docs.crunch.io/endpoint-reference/endpoint-variable.html#post-catalog) #nolint
+#' "description". See the [API documentation](
+#' http://docs.crunch.io/endpoint-reference/endpoint-variable.html#post-catalog)
 #' for a complete list of valid attributes.
 #' @return A `VariableDefinition` object. To add this to a dataset, either
 #' assign it into the dataset (like `ds$newvar <- toVariable(...)`) or call
