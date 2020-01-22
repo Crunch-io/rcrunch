8a75beed378599fe9149bd862cf0cf066e3b40d7
Greg Freedman Ellis <greg@crunch.io>
2020-01-22 16:31:22 -0600

    fork and merge vignette working now?

--- vignettes/fork-and-merge/0/api/sources-POST.R
+++ vignettes/fork-and-merge/0/api/sources-POST.R
@@ -1,20 +1,20 @@
 structure(list(url = "/api/sources/", status_code = 201L, headers = structure(list(
-    allow = "GET, HEAD, OPTIONS, POST", `content-encoding` = "gzip", 
-    `content-type` = "application/json;charset=utf-8", date = "Mon, 14 May 2018 16:51:20 GMT", 
-    location = "/api/sources/6d5344/", server = "nginx", `set-cookie` = "REDACTED", 
-    vary = "Cookie, Accept-Encoding", `x-timing` = "", `content-length` = "86", 
-    connection = "keep-alive"), class = c("insensitive", "list"
-)), all_headers = list(list(status = 201L, version = "HTTP/1.1", 
-    headers = structure(list(allow = "GET, HEAD, OPTIONS, POST", 
-        `content-encoding` = "gzip", `content-type` = "application/json;charset=utf-8", 
-        date = "Mon, 14 May 2018 16:51:20 GMT", location = "/api/sources/6d5344/", 
-        server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
-        `x-timing` = "", `content-length` = "86", connection = "keep-alive"), class = c("insensitive", 
+    date = "Wed, 22 Jan 2020 22:28:14 GMT", `content-type` = "application/json;charset=utf-8", 
+    `content-length` = "86", location = "/api/sources/ac2ed0/", 
+    server = "nginx", `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+    allow = "GET, HEAD, OPTIONS, POST", `x-timing` = "", `set-cookie` = "REDACTED"), class = c("insensitive", 
+"list")), all_headers = list(list(status = 201L, version = "HTTP/2", 
+    headers = structure(list(date = "Wed, 22 Jan 2020 22:28:14 GMT", 
+        `content-type` = "application/json;charset=utf-8", `content-length` = "86", 
+        location = "/api/sources/ac2ed0/", server = "nginx", 
+        `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+        allow = "GET, HEAD, OPTIONS, POST", `x-timing` = "", 
+        `set-cookie` = "REDACTED"), class = c("insensitive", 
     "list")))), cookies = structure(list(domain = ".crunch.io", 
-    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1557852680, class = c("POSIXct", 
+    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1611268095, class = c("POSIXct", 
     "POSIXt")), name = "token", value = "REDACTED"), row.names = c(NA, 
 -1L), class = "data.frame"), content = charToRaw("{\"element\": \"shoji:view\", \"self\": \"/api/sources/\"}"), 
-    date = structure(1526316680, class = c("POSIXct", "POSIXt"
-    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 2.7e-05, 
-    connect = 2.9e-05, pretransfer = 0.000158, starttransfer = 0.000172, 
-    total = 1.711155)), class = "response")
+    date = structure(1579732094, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 2.1e-05, 
+    connect = 2.2e-05, pretransfer = 0.000114, starttransfer = 0.000118, 
+    total = 1.121258)), class = "response")
