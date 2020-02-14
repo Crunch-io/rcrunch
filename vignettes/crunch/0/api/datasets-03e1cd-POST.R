3a11756a4f1238284a8050374839fdd5215ecba1
Greg Freedman Ellis <greg@crunch.io>
2020-02-14 12:20:21 -0600

    recache vignette mocks

--- vignettes/crunch/0/api/datasets-03e1cd-POST.R
+++ vignettes/crunch/0/api/datasets-03e1cd-POST.R
@@ -1,19 +1,21 @@
-structure(list(url = "/api/datasets/", status_code = 201L, headers = structure(list(
-    allow = "GET, HEAD, OPTIONS, PATCH, POST", `content-encoding` = "gzip", 
-    `content-type` = "application/json;charset=utf-8", date = "Thu, 02 Aug 2018 21:33:19 GMT", 
-    location = "/api/datasets/1468ef/", server = "nginx", `set-cookie` = "REDACTED", 
-    vary = "Cookie, Accept-Encoding", `x-timing` = "", `content-length` = "24", 
-    connection = "keep-alive"), class = c("insensitive", "list"
-)), all_headers = list(list(status = 201L, version = "HTTP/1.1", 
-    headers = structure(list(allow = "GET, HEAD, OPTIONS, PATCH, POST", 
-        `content-encoding` = "gzip", `content-type` = "application/json;charset=utf-8", 
-        date = "Thu, 02 Aug 2018 21:33:19 GMT", location = "/api/datasets/1468ef/", 
-        server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
-        `x-timing` = "", `content-length` = "24", connection = "keep-alive"), class = c("insensitive", 
+structure(list(url = "/api/datasets/", status_code = 202L, headers = structure(list(
+    date = "Fri, 14 Feb 2020 17:50:02 GMT", `content-type` = "application/json;charset=utf-8", 
+    `content-length` = "177", location = "/api/datasets/f32c24/", 
+    server = "nginx", `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+    allow = "GET, HEAD, OPTIONS, PATCH, POST", `x-timing` = "", 
+    `set-cookie` = "REDACTED"), class = c("insensitive", "list"
+)), all_headers = list(list(status = 202L, version = "HTTP/2", 
+    headers = structure(list(date = "Fri, 14 Feb 2020 17:50:02 GMT", 
+        `content-type` = "application/json;charset=utf-8", `content-length` = "177", 
+        location = "/api/datasets/f32c24/", server = "nginx", 
+        `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+        allow = "GET, HEAD, OPTIONS, PATCH, POST", `x-timing` = "", 
+        `set-cookie` = "REDACTED"), class = c("insensitive", 
     "list")))), cookies = structure(list(domain = ".crunch.io", 
-    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1564781599, class = c("POSIXct", 
+    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1613238602, class = c("POSIXct", 
     "POSIXt")), name = "token", value = "REDACTED"), row.names = c(NA, 
--1L), class = "data.frame"), content = charToRaw("null"), date = structure(1533245599, class = c("POSIXct", 
-"POSIXt"), tzone = "GMT"), times = c(redirect = 0, namelookup = 4.1e-05, 
-connect = 4.3e-05, pretransfer = 0.000492, starttransfer = 5e-04, 
-total = 1.127142)), class = "response")
+-1L), class = "data.frame"), content = charToRaw("{\"element\": \"shoji:view\", \"self\": \"/api/datasets/\", \"value\": \"/api/progress/\"}"), 
+    date = structure(1581702602, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 2.9e-05, 
+    connect = 3e-05, pretransfer = 9e-05, starttransfer = 9.4e-05, 
+    total = 1.442158)), class = "response")
--- vignettes/fork-and-merge/3/api/datasets-2ea7ca-POST.R
+++ vignettes/fork-and-merge/3/api/datasets-2ea7ca-POST.R
@@ -1,19 +1,21 @@
-structure(list(url = "/api/datasets/", status_code = 201L, headers = structure(list(
-    allow = "GET, HEAD, OPTIONS, PATCH, POST", `content-encoding` = "gzip", 
-    `content-type` = "application/json;charset=utf-8", date = "Mon, 14 May 2018 16:51:31 GMT", 
-    location = "/api/datasets/a1548c/", server = "nginx", `set-cookie` = "REDACTED", 
-    vary = "Cookie, Accept-Encoding", `x-timing` = "", `content-length` = "24", 
-    connection = "keep-alive"), class = c("insensitive", "list"
-)), all_headers = list(list(status = 201L, version = "HTTP/1.1", 
-    headers = structure(list(allow = "GET, HEAD, OPTIONS, PATCH, POST", 
-        `content-encoding` = "gzip", `content-type` = "application/json;charset=utf-8", 
-        date = "Mon, 14 May 2018 16:51:31 GMT", location = "/api/datasets/a1548c/", 
-        server = "nginx", `set-cookie` = "REDACTED", vary = "Cookie, Accept-Encoding", 
-        `x-timing` = "", `content-length` = "24", connection = "keep-alive"), class = c("insensitive", 
+structure(list(url = "/api/datasets/", status_code = 202L, headers = structure(list(
+    date = "Fri, 14 Feb 2020 18:07:03 GMT", `content-type` = "application/json;charset=utf-8", 
+    `content-length` = "176", location = "/api/datasets/08b84d/", 
+    server = "nginx", `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+    allow = "GET, HEAD, OPTIONS, PATCH, POST", `x-timing` = "", 
+    `set-cookie` = "REDACTED"), class = c("insensitive", "list"
+)), all_headers = list(list(status = 202L, version = "HTTP/2", 
+    headers = structure(list(date = "Fri, 14 Feb 2020 18:07:03 GMT", 
+        `content-type` = "application/json;charset=utf-8", `content-length` = "176", 
+        location = "/api/datasets/08b84d/", server = "nginx", 
+        `content-encoding` = "gzip", vary = "Cookie, Accept-Encoding", 
+        allow = "GET, HEAD, OPTIONS, PATCH, POST", `x-timing` = "", 
+        `set-cookie` = "REDACTED"), class = c("insensitive", 
     "list")))), cookies = structure(list(domain = ".crunch.io", 
-    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1557852691, class = c("POSIXct", 
+    flag = TRUE, path = "/", secure = FALSE, expiration = structure(1613239623, class = c("POSIXct", 
     "POSIXt")), name = "token", value = "REDACTED"), row.names = c(NA, 
--1L), class = "data.frame"), content = charToRaw("null"), date = structure(1526316691, class = c("POSIXct", 
-"POSIXt"), tzone = "GMT"), times = c(redirect = 0, namelookup = 4.5e-05, 
-connect = 4.8e-05, pretransfer = 0.000721, starttransfer = 0.00073, 
-total = 1.058085)), class = "response")
+-1L), class = "data.frame"), content = charToRaw("{\"element\": \"shoji:view\", \"self\": \"/api/datasets/\", \"value\": \"/api/progress/\"}"), 
+    date = structure(1581703623, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 3.1e-05, 
+    connect = 3.2e-05, pretransfer = 9.9e-05, starttransfer = 0.000104, 
+    total = 1.016113)), class = "response")
