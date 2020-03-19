867d27aee0bddd6d409dc5cf1b30b4b834578d73
Greg Freedman Ellis <greg@crunch.io>
2020-03-19 09:31:12 -0500

    start makeIntegrations switch to zcl function (collapse_missing arg not yet working)

--- R/interact.R
+++ R/interact.R
@@ -11,8 +11,6 @@
 #' as well as other properties to pass about the case variable (i.e. alias,
 #'  description)
 #' @param name a character to use as the name for the interaction variable
-#' @param sep a character to separate the values of the individual variables
-#'  (default: `:`)
 #'
 #' @return A [`VariableDefinition`] that creates the new interaction variable.
 #' @examples
@@ -22,57 +20,27 @@
 #' )
 #' }
 #' @export
-interactVariables <- function(..., name, sep = ":") {
+interactVariables <- function(..., name, collapse_missings = FALSE) {
     dots <- list(...)
     vars <- Filter(is.variable, dots)
     other_dots <- Filter(Negate(is.variable), dots)
+
+    if ("sep" %in% names(other_dots)) {
+        warning("The `sep` argument is no longer supported.")
+        other_dots$sep <- NULL
+    }
 
     if (length(vars) < 2) {
         halt("must supply more than one variable to make an interaction")
     }
 
-    case_args <- c(vars, sep = sep)
-    cases <- do.call(makeInteractions, case_args)
+    other_dots$name <- name
+    other_dots$derivation <- zfunc(
+        "interact_categories", 
+        lapply(vars, function(x) list(variable = urls(x))),
+        list(value = collapse_missings)
+    )
 
-    int_args <- c(cases = list(cases), name = name, other_dots)
-    interactionVar <- do.call(makeCaseVariable, int_args)
-    return(interactionVar)
+    do.call(VariableDefinition, other_dots)
 }
 
-#' Make case statements that define the interaction of two or more other variables
-#'
-#' @param ... A sequence of categorical variables to make an interaction of
-#' @param sep a character to separate the values of the individual variables
-#'
-#' @return A list of cases that can be passed to [`makeCaseVariable`] to make
-#' the interaction variable.
-#'
-#' @keywords internal
-makeInteractions <- function(..., sep) {
-    vars <- list(...)
-    names(vars) <- lapply(vars, alias)
-
-    # check that vars are categorical
-    if (any(!vapply(vars, is.Categorical, logical(1)))) {
-        halt("makeInteractions can only take categorical variables")
-    }
-
-    cats <- lapply(vars, categories)
-    all_combos <- expand.grid(cats)
-
-    cases <- by(all_combos, seq(nrow(all_combos)), makeCase, vars = vars, sep = sep)
-
-    # remove attributes/names
-    attributes(cases) <- NULL
-
-    return(cases = cases)
-}
-
-makeCase <- function(df, vars, sep) {
-    exprs <- mapply(function(cat, var) var == names(cat), df, vars)
-    nms <- lapply(df, names)
-    name <- paste(nms, collapse = sep)
-    msng <- any(vapply(df, is.na, logical(1)))
-
-    list(expression = Reduce("&", exprs), name = name, missing = msng)
-}
