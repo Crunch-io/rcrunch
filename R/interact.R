3754896ca065ce0272985b448e37b8e5dca2431c
Greg Freedman Ellis <greg@crunch.io>
2020-03-23 11:59:13 -0500

    api fix has made it to master

--- R/interact.R
+++ R/interact.R
@@ -36,13 +36,12 @@ interactVariables <- function(..., name, collapse_missings = FALSE) {
 
     other_dots$name <- name
     other_dots$derivation <- zfunc(
-        "interact_categories", 
+        "interact_categories",
         # unnaming prevents JSON request object getting numeric descriptors
-        unname(lapply(vars, function(x) list(variable = urls(x))))#, 
-        # list(value = collapse_missings) # if statement is temporary work around for https://www.pivotaltracker.com/story/show/171882266
+        unname(lapply(vars, function(x) list(variable = urls(x)))),
+        list(value = collapse_missings)
     )
-    if (collapse_missings) other_dots$derivation$args[[2]] <- list(value = collapse_missings)
-    
+
     do.call(VariableDefinition, other_dots)
 }
 
