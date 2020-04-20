ce37f0866215764983dea249f93720b87a9545f8
Greg Freedman Ellis <greg@crunch.io>
2020-04-20 14:27:24 -0500

    temporary fix to invalid xml

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,32 @@
 library(httptest)
 options(crunch.check.updates = FALSE)
 if (nchar(Sys.getenv("JENKINS_HOME"))) {
+    tt_read_lines <- function (path, n = -1L, encoding = "UTF-8") {
+        base::readLines(path, n = n, encoding = encoding, warn = FALSE)
+    }
+
+    tt_write_lines <- function (text, path) {
+        base::writeLines(enc2utf8(text), path, useBytes = TRUE)
+    }
+
+    JunitReporterFix <- R6::R6Class(
+        "JunitReporterFix",
+        inherit = JunitReporter,
+        public = list(
+            end_reporter = function() {
+                super$end_reporter()
+                tt_write_lines(
+                    crayon::strip_style(tt_read_lines(self$out)),
+                    self$out
+                )
+            }
+        )
+    )
+
     test_check("crunch",
         reporter = MultiReporter$new(list(
             SummaryReporter$new(),
-            JunitReporter$new(file = file.path(Sys.getenv("WORKSPACE"), "rcrunch.xml"))
+            JunitReporterFix$new(file = file.path(Sys.getenv("WORKSPACE"), "rcrunch.xml"))
         ))
     )
 } else {
