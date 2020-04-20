22cc9be226528367f469b800ef2208aa4ab32b39
Greg Freedman Ellis <greg@crunch.io>
2020-04-20 16:49:45 -0500

    friendly lintrbot

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,11 @@
 library(httptest)
 options(crunch.check.updates = FALSE)
 if (nchar(Sys.getenv("JENKINS_HOME"))) {
-    tt_read_lines <- function (path, n = -1L, encoding = "UTF-8") {
+    tt_read_lines <- function(path, n = -1L, encoding = "UTF-8") {
         base::readLines(path, n = n, encoding = encoding, warn = FALSE)
     }
 
-    tt_write_lines <- function (text, path) {
+    tt_write_lines <- function(text, path) {
         base::writeLines(enc2utf8(text), path, useBytes = TRUE)
     }
 
