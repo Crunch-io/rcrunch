a923dcb3d7b218c1ffb7273ffbb13a001b6f3940
Greg Freedman Ellis <greg@crunch.io>
2020-07-17 14:27:56 -0500

    update version num & news

--- inst/httptest/redact.R
+++ inst/httptest/redact.R
@@ -6,12 +6,15 @@ function(response) {
     if (response$url == "https://app.crunch.io/api/public/login/") {
         response$request$options[["postfields"]] <- NULL
     }
-    response %>%
+    out <- response %>%
         redact_auth() %>%
         gsub_response("([0-9a-f]{6})[0-9a-f]{26}", "\\1") %>% ## Prune UUIDs
-        httptest::gsub_response("[0-9A-Za-z]{22}([0-9]{6})", "\\1") %>% # UUIDs in variables now too
+        gsub_response("[0-9A-Za-z]{22}([0-9]{6})", "\\1") %>% # UUIDs in variables now too
         gsub_response(
             "https.//app.crunch.io/api/progress/[^\"].*?/",
             "https://app.crunch.io/api/progress/"
         )
+
+    if (grepl("%20", out$request$url) | grepl(" ", out$request$url)) browser()
+    out
 }
