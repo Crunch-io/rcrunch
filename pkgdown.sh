50b126721414bf84ca446f695fa2ec0708436370
Greg Freedman Ellis <greg@crunch.io>
2020-11-16 13:13:28 -0600

    try out pkgdown from gh actions

--- pkgdown.sh
+++ pkgdown.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
-    [ "${TRAVIS_TAG}" != "" ] &&
-    [ "${TRAVIS_R_VERSION_STRING}" = "release" ]; then
-
-    R CMD INSTALL . ## Package needs to be installed, apparently
-    R -e 'remotes::install_github("r-lib/pkgdown"); pkgdown::build_site()'
-    git clone --branch src https://${GH_TOKEN}@github.com/Crunch-io/ta-da.git ../ta-da
-    rm -rf ../ta-da/static/r/crunch
-    cp -r docs/. ../ta-da/static/r/crunch
-    cd ../ta-da
-    git add .
-    git commit -m "Updating rcrunch pkgdown site (release ${TRAVIS_TAG})" || true
-    git push origin src || true
+git clone --branch src https://${GH_TOKEN}@github.com/Crunch-io/ta-da.git ../ta-da
+rm -rf ../ta-da/static/r/crunch
+cp -r docs/. ../ta-da/static/r/crunch
+cd ../ta-da
+git add .
+git commit -m "Updating rcrunch pkgdown site (release ${RELEASE_VERSION})" || true
+git push origin src || true
 fi
