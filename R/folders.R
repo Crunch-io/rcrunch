3527e4c9c69c0e9c6d6d3488f78b994e85711ec1
Greg Freedman Ellis <greg@crunch.io>
2021-04-13 13:01:13 -0500

    WIP generateOrganizeCommand to get the Automation script for current folder order

--- R/folders.R
+++ R/folders.R
@@ -385,6 +385,109 @@ copyFolders <- function(source, target) {
 }
 
 
+# Recursively go through folders and create crunch automation commands
+organize_recurse <- function(base, base_type, path) {
+    items <- cd(base, path)
+
+    # Can't make empty folders in crunch automation
+    if (length(items) == 0) {
+        return()
+    }
+
+    if (length(items) == 1) {
+        split_items <- list(items)
+    } else {
+        # Make sequences where each folder is on its own, and the variables
+        # at the folder level are in sequence. This allows the organize folder
+        # to go in the right place so that the folder is organized among the vas
+        folder_pos <- which(types(items) == "folder")
+        # starts are 1, the folder positions, and 1 after the folder positions so that
+        # each folder gets split out on its own. If position is to high, needs to be
+        # replaced with length, which can happen if a folder is in last position
+        starts <- pmin(c(1, folder_pos + c(0, 1)), length(items))
+        # and ends are one less than the folders, the folder positions and the length of
+        # the list. Need to replace item less than 1, which can occur if folder is in
+        # first position
+        ends <- pmax(c(folder_pos - c(1, 0), length(items)), 1)
+
+        split_items <- mapply(
+            FUN = function(starts, ends) items[seq(starts, ends)],
+            starts = starts,
+            ends = ends
+        )
+    }
+
+    lapply(
+        split_items,
+        organize_recurse_cmd,
+        path = path,
+        base = base,
+        base_type = base_type
+    )
+
+}
+
+# Generate commands for organizing, and recurse into folder if necessary
+organize_recurse_cmd <- function(items, path, base, base_type) {
+    if (length(items) == 1 && types(items) == "folder") {
+        below_path <- paste0(path, name(items[[1]]), "/")
+        organize_recurse(base, base_type, below_path)
+    } else {
+        aliases <- paste0(validate_automation_aliases(aliases(items)), collapse = ", ")
+        path <- path_to_automation_path(path, base_type)
+        paste0("ORGANIZE ", aliases, " INTO ", path, ";")
+    }
+}
+
+# Convert from a rcrunch path to a Crunch Automation path
+path_to_automation_path <- function(path, base_type) {
+    path <- gsub("^/?(.*?)/?$", "\\1", path)
+    path <- gsub("/", "|", path)
+    if (path == "" && base_type != "ROOT") {
+        return(base_type)
+    }
+
+    if (base_type == "ROOT") {
+        base_type <- ""
+    } else {
+        base_type <- paste0(base_type, " ")
+    }
+    paste0(base_type, '"', path, '"')
+}
+
+# decide whether to put backticks on variable aliases
+validate_automation_aliases <- function(x) {
+    # Can't start with a number or have non-alphanumeric characters
+    # (may be overly cautious, but doesn't do backticks on clearly good aliases
+    invalid <- grepl("^[0-9]|[^[:alnum:]_]", x)
+
+    x[invalid] <- paste0("`", x[invalid], "`")
+    x
+}
+
+#' Generate Crunch Automation commands to create a dataset's current folder struct
+#'
+#' Take a datset and generate the Crunch Automation commands needed to create the
+#' current folder structure of the dataset. Useful for saving a snapshot, or copying
+#' one dataset's folder structure to another (though see [`copyFolders()`] for another
+#' way of copying a dataset's folders).
+#'
+#' @param dataset A crunch Dataset
+#'
+#' @return A string of the commands that can be passed to [`runCrunchAutomation()`]
+#' @export
+generateOrganizeCommand <- function(dataset) {
+    if (!is.dataset(dataset) ) {
+        halt("dataset must be a Crunch dataset.")
+    }
+    root_cmds <- organize_recurse(dataset, "ROOT", "/")
+    hidden_cmds <- organize_recurse(hiddenFolder(dataset), "HIDDEN", "/")
+    secure_cmds <- organize_recurse(privateFolder(dataset), "PRIVATE", "/")
+
+    paste0(c(unlist(root_cmds), unlist(hidden_cmds), unlist(secure_cmds)), collapse = "\n")
+}
+
+
 # Recursively get all variables below a folder
 # TODO: Use trampoline? https://community.rstudio.com/t/tidiest-way-to-do-recursion-safely-in-r/1408
 # My initial tests say it's slower, but is safer if we ever expect a large number of folders
