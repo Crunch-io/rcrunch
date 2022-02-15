5eacb5018fce1b060750f2d0468aacc84002e6c9
Greg Freedman Ellis <greg@crunch.io>
2022-02-15 14:08:19 -0600

    [181268334]: clear cache

--- inst/crunch-test.R
+++ inst/crunch-test.R
@@ -133,6 +133,7 @@ with_test_authentication <- function(expr) {
         with(test_options, {
             ## Authenticate.
             on.exit({
+                httpcache::clearCache()
                 ## Delete our seen things
                 purgeEntitiesCreated()
             })
