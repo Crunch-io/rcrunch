9dfae498f5170384be1fb4c0b248886da7d55cb4
Greg Freedman Ellis <greg@crunch.io>
2024-11-08 12:35:04 -0600

    [188532827]: fix bad test option

--- inst/crunch-test.R
+++ inst/crunch-test.R
@@ -153,7 +153,7 @@ test_options <- temp.options(
             "test.api",
             "http://local.crunch.io:8080/api/"
         ),
-        crunch.api.key = Sys.getenv("CRUNCH_TEST_API_KEY"),
+        crunch.api.key = envOrOption("test.api.key"),
         crunch.show.progress = FALSE,
         crunch.verify_ssl = crunch::envOrOption("test.verify_ssl", TRUE, expect_lgl = TRUE),
         message.auth.info = TRUE
